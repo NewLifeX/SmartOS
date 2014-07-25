d055453ba4a28a1a5c5a6bef22c7e22a06e32e7b
Stone <nnhy@vip.qq.com>
2014-07-25 09:59:34 +0000

    为了确保对GD的兼容，先初始化一次结构体

--- Spi.cpp
+++ Spi.cpp
@@ -81,15 +81,16 @@ Spi::Spi(int spi, int speedHz, bool useNss)
 	Stop();
 
 	SPI_InitTypeDef sp;
-	sp.SPI_Direction = SPI_Direction_2Lines_FullDuplex;  //设置SPI单向或者双向的数据模式:SPI设置为双线双向全双工
-	sp.SPI_Mode = SPI_Mode_Master;		//设置SPI工作模式:设置为主SPI
-	sp.SPI_DataSize = SPI_DataSize_8b;		//设置SPI的数据大小:SPI发送接收8位帧结构
-	sp.SPI_CPOL = SPI_CPOL_Low;		//选择了串行时钟的稳态:时钟悬空高
-	sp.SPI_CPHA = SPI_CPHA_1Edge;	//数据捕获于第二个时钟沿
-	sp.SPI_NSS = SPI_NSS_Soft;		//NSS信号由硬件（NSS管脚）还是软件（使用SSI位）管理:内部NSS信号有SSI位控制
-	sp.SPI_BaudRatePrescaler = pre;		//定义波特率预分频的值:波特率预分频值为
-	sp.SPI_FirstBit = SPI_FirstBit_MSB;	//指定数据传输从MSB位还是LSB位开始:数据传输从MSB位开始
-	sp.SPI_CRCPolynomial = 7;	//CRC值计算的多项式
+    SPI_StructInit(&sp);
+	sp.SPI_Direction = SPI_Direction_2Lines_FullDuplex; //双线全双工
+	sp.SPI_Mode = SPI_Mode_Master;      // 主模式
+	sp.SPI_DataSize = SPI_DataSize_8b;  // 数据大小8位 SPI发送接收8位帧结构
+	sp.SPI_CPOL = SPI_CPOL_Low;         // 时钟极性，空闲时为低
+	sp.SPI_CPHA = SPI_CPHA_1Edge;       // 第1个边沿有效，上升沿为采样时刻
+	sp.SPI_NSS = SPI_NSS_Soft;          // NSS信号由硬件（NSS管脚）还是软件（使用SSI位）管理:内部NSS信号有SSI位控制
+	sp.SPI_BaudRatePrescaler = pre;     // 8分频，9MHz 定义波特率预分频的值
+	sp.SPI_FirstBit = SPI_FirstBit_MSB; // 高位在前。指定数据传输从MSB位还是LSB位开始:数据传输从MSB位开始
+	sp.SPI_CRCPolynomial = 7;           // CRC值计算的多项式
 
     SPI_Init(spiport, &sp);
     SPI_Cmd(spiport, ENABLE);
