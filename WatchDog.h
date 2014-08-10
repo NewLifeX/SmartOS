9b23d917163d4e0b5fd3b6b5495002155d7bd947
nnhy <nnhy@vip.qq.com>
2014-08-10 06:46:48 +0000

    看门狗测试通过

--- WatchDog.h
+++ WatchDog.h
@@ -9,6 +9,14 @@ class WatchDog
 private:
 
 public:
+	WatchDog(uint ms = 3000);
+	~WatchDog();
+	
+	uint Timeout; // 当前超时时间
+	
+	bool Config(uint ms);	// 配置看门狗喂狗重置时间，超过该时间讲重启MCU
+	void ConfigMax();		// 看门狗无法关闭，只能设置一个最大值
+	void Feed(); // 喂狗
 };
 
 #endif
