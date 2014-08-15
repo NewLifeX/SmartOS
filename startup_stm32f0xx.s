7b008eccd6ac023ca50374a6812fe363fba28e0a
Stone <nnhy@vip.qq.com>
2014-08-15 13:07:33 +0000

    修正RETURN异常的问题，原来是因为分配堆空间失败 因为SmartOS需要大量分配内存，这里设定一个较大的值，在内存不足的项目可改小

--- startup_stm32f0xx.s
+++ startup_stm32f0xx.s
@@ -1,15 +1,12 @@
+; 栈空间意义不大，SmartOS将会重新设定到RAM最大值
 Stack_Size      EQU     0x00000400
 
                 AREA    STACK, NOINIT, READWRITE, ALIGN=3
 Stack_Mem       SPACE   Stack_Size
 __initial_sp
 
-
-; <h> Heap Configuration
-;   <o>  Heap Size (in Bytes) <0x0-0xFFFFFFFF:8>
-; </h>
-
-Heap_Size       EQU     0x00000200
+; 因为SmartOS需要大量分配内存，这里设定一个较大的值，在内存不足的项目可改小
+Heap_Size       EQU     0x00000800
 
                 AREA    HEAP, NOINIT, READWRITE, ALIGN=3
 __heap_base
@@ -35,15 +32,15 @@ __Vectors_Size  EQU  __Vectors_End - __Vectors
                 AREA    |.text|, CODE, READONLY
 
 ; Reset handler routine
-Reset_Handler    PROC
-                 EXPORT  Reset_Handler                 [WEAK]
-        IMPORT  __main
-        IMPORT  SystemInit  
-                 LDR     R0, =SystemInit
-                 BLX     R0
-                 LDR     R0, =__main
-                 BX      R0
-                 ENDP
+Reset_Handler   PROC
+                EXPORT  Reset_Handler                 [WEAK]
+				IMPORT  __main
+				IMPORT  SystemInit  
+                LDR     R0, =SystemInit
+                BLX     R0
+                LDR     R0, =__main
+                BX      R0
+                ENDP
 
                 ALIGN
 
--- startup_stm32f10x_hd.s
+++ startup_stm32f10x_hd.s
@@ -1,14 +1,12 @@
+; 栈空间意义不大，SmartOS将会重新设定到RAM最大值
 Stack_Size      EQU     0x00000400
 
                 AREA    STACK, NOINIT, READWRITE, ALIGN=3
 Stack_Mem       SPACE   Stack_Size
 __initial_sp
-                                                  
-; <h> Heap Configuration
-;   <o>  Heap Size (in Bytes) <0x0-0xFFFFFFFF:8>
-; </h>
 
-Heap_Size       EQU     0x00000200
+; 因为SmartOS需要大量分配内存，这里设定一个较大的值，在内存不足的项目可改小	。堆空间不足时malloc讲引发异常
+Heap_Size       EQU     0x00004000	
 
                 AREA    HEAP, NOINIT, READWRITE, ALIGN=3
 __heap_base
@@ -32,14 +30,14 @@ __Vectors_End
 __Vectors_Size  EQU  __Vectors_End - __Vectors
 
                 AREA    |.text|, CODE, READONLY
-                
+
 ; Reset handler
 Reset_Handler   PROC
                 EXPORT  Reset_Handler             [WEAK]
                 IMPORT  __main
                 IMPORT  SystemInit
                 LDR     R0, =SystemInit
-                BLX     R0               
+                BLX     R0
                 LDR     R0, =__main
                 BX      R0
                 ENDP
@@ -50,16 +48,16 @@ Reset_Handler   PROC
 ; User Stack and Heap initialization
 ;*******************************************************************************
                  IF      :DEF:__MICROLIB
-                
+
                  EXPORT  __initial_sp
                  EXPORT  __heap_base
                  EXPORT  __heap_limit
-                
+
                  ELSE
-                
+
                  IMPORT  __use_two_region_memory
                  EXPORT  __user_initial_stackheap
-                 
+
 __user_initial_stackheap
 
                  LDR     R0, =  Heap_Mem
