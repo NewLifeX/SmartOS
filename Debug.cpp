77920dd1e2a0338f0aafec5e67c9dc36134fa4b3
Stone <nnhy@vip.qq.com>
2014-08-19 15:02:03 +0000

    完善内存预警，测试通过

--- Debug.cpp
+++ Debug.cpp
@@ -3,11 +3,22 @@
 
 #if DEBUG
 
+extern uint __heap_base;
+extern uint __heap_limit;
+
 void* operator new(uint size)
 {
     debug_printf(" new size: %d ", size);
     void * p = malloc(size);
-	if(!p) debug_printf("malloc failed! size=%d", size);
+	if(!p)
+		debug_printf("malloc failed! size=%d ", size);
+	else
+	{
+		debug_printf("0x%08x ", p);
+		// 如果堆只剩下64字节，则报告失败，要求用户扩大堆空间以免不测
+		uint end = (uint)&__heap_limit;
+		if((uint)p + size + 0x40 >= end) debug_printf(" + %d near HeapEnd=0x%08x", size, end);
+	}
 	assert_param(p);
     return p;
 }
@@ -16,7 +27,15 @@ void* operator new[](uint size)
 {
     debug_printf(" new size[]: %d ", size);
     void * p = malloc(size);
-	if(!p) debug_printf("malloc failed! size=%d", size);
+	if(!p)
+		debug_printf("malloc failed! size=%d ", size);
+	else
+	{
+		debug_printf("0x%08x ", p);
+		// 如果堆只剩下64字节，则报告失败，要求用户扩大堆空间以免不测
+		uint end = (uint)&__heap_limit;
+		if((uint)p + size + 0x40 >= end) debug_printf(" + %d near HeapEnd=0x%08x", size, end);
+	}
 	assert_param(p);
     return p;
 }
