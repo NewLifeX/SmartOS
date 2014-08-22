3e7645378a1e5d5b09d3a4334b0d8bd2a5d311e7
Stone <nnhy@vip.qq.com>
2014-08-22 07:56:34 +0000

    引入SmartOSF4例程

--- stm32.h
+++ stm32.h
@@ -1,4 +1,4 @@
-﻿#ifndef __STM32_H
+#ifndef __STM32_H
 #define __STM32_H
 
 #ifdef STM32F0XX_LD
@@ -13,15 +13,45 @@
     #endif
 #endif
 
-#if defined(STM32F4XX)
+#ifdef STM32F0XX
+    #ifndef STM32F0
+        #define STM32F0
+    #endif
+#else
+    #ifdef STM32F0
+        #define STM32F0XX
+    #endif
+#endif
+
+#ifdef STM32F10X
+    #ifndef STM32F1
+        #define STM32F1
+    #endif
+#else
+    #ifdef STM32F1
+        #define STM32F10X
+    #endif
+#endif
+
+#ifdef STM32F4XX
+    #ifndef STM32F4
+        #define STM32F4
+    #endif
+#else
+    #ifdef STM32F4
+        #define STM32F4XX
+    #endif
+#endif
+
+#if defined(STM32F4)
 	#include "stm32f4xx.h"
-#elif defined(STM32F2XX)
+#elif defined(STM32F2)
 	#include "stm32f2xx.h"
-#elif defined(STM32F10X)
+#elif defined(STM32F1)
 	#include "stm32f10x.h"
-#elif defined(STM32F3XX)
+#elif defined(STM32F3)
 	#include "stm32f3xx.h"
-#elif defined(STM32F0XX)
+#elif defined(STM32F0)
 	#include "stm32f0xx.h"
 #else
 	#include "stm32f10x.h"
