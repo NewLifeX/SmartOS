799d0951d07b762caac5e99de60db5ac03b76537
Stone <nnhy@vip.qq.com>
2014-08-29 04:17:59 +0000

    增加Zigbee协议，主站发送

---
+++ Net/Zigbee.h
@@ -0,0 +1,42 @@
+﻿#ifndef __Zigbee_H__
+#define __Zigbee_H__
+
+#include "Sys.h"
+#include "Port.h"
+#include "Net\ITransport.h"
+
+// Zigbee协议
+// 主站发送所有从站收到，从站发送只有主站收到
+class Zigbee : public ITransport
+{
+private:
+	ITransport* _port;
+	OutputPort* _rst;
+
+public:
+	Zigbee(ITransport* port, Pin rst = P0);
+	virtual ~Zigbee();
+
+	// 注册回调函数
+	virtual void Register(TransportHandler handler, void* param = NULL)
+	{
+		ITransport::Register(handler, param);
+		
+		_port->Register(OnPortReceive, this);
+	}
+
+protected:
+	virtual bool OnOpen() { return _port->Open(); }
+    virtual void OnClose() { _port->Close(); }
+
+    virtual bool OnWrite(const byte* buf, uint len) { return _port->Write(buf, len); }
+	virtual uint OnRead(byte* buf, uint len) { return _port->Read(buf, len); }
+	
+	static uint OnPortReceive(ITransport* sender, byte* buf, uint len, void* param)
+	{
+		Zigbee* zb = (Zigbee*)param;
+		return zb->OnReceive(buf, len);
+	}
+};
+
+#endif
