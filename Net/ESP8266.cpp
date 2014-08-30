d5cb746fe2807b8d418db7db862003d3340c0c4a
Stone <nnhy@vip.qq.com>
2014-08-30 04:55:20 +0000

    上海乐鑫ESP8266，WiFi模块

---
+++ Net/ESP8266.cpp
@@ -0,0 +1,20 @@
+#include "Zigbee.h"
+
+Zigbee::Zigbee(ITransport* port, Pin rst)
+{
+	assert_param(port);
+
+	_port = port;
+	
+	_rst = NULL;
+	if(rst != P0) _rst = new OutputPort(rst);
+}
+
+Zigbee::~Zigbee()
+{
+	if(_port) delete _port;
+	_port = NULL;
+	
+	if(_rst) delete _rst;
+	_rst = NULL;
+}
