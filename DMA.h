148da8116c968eea9095a0786d18332228c59129
Stone <nnhy@vip.qq.com>
2014-09-08 13:18:21 +0000

    部分DMA封装，未完成

---
+++ DMA.h
@@ -0,0 +1,32 @@
+#ifndef __DMA_H__
+#define __DMA_H__
+
+#include "Sys.h"
+
+// DMA
+class DMA
+{
+private:
+	byte _index;	// 第几个定时器，从0开始
+	bool _started;
+	
+public:
+	DMA(byte index);
+	~DMA();
+	
+    int Retry;  // 等待重试次数，默认200
+    int Error;  // 错误次数
+	
+	bool Start();	// 开始
+	bool WaitForStop();	// 停止
+	
+	//typedef void (*TimerHandler)(Timer* tim, void* param);
+	//void Register(TimerHandler handler, void* param = NULL);
+
+private:
+	//static void OnHandler(ushort num, void* param);
+	//TimerHandler _Handler;
+	//void* _Param;
+};
+
+#endif
