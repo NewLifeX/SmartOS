a031e2c8a5eecd127e1610edc50b9a7af29c3902
Stone <nnhy@vip.qq.com>
2014-09-25 08:23:06 +0000

    Message初步编译通过，未测试

--- TinyNet/Message.cpp
+++ TinyNet/Message.cpp
@@ -1,4 +1,4 @@
-#include "Message.h"
+﻿#include "Message.h"
 
 // 初始化消息，各字段为0
 void Message::Init()
@@ -7,31 +7,38 @@ void Message::Init()
 }
 
 // 分析数据，转为消息。负载数据部分将指向数据区，外部不要提前释放内存
-void Message::Parse(byte* buf, uint len)
+bool Message::Parse(byte* buf, uint len)
 {
 	assert_ptr(buf);
 	assert_param(len > 0);
 
 	// 消息至少4个头部字节和2个校验字节，没有负载数据的情况下
 	const int headerSize = 4 + 2;
-	if(len < headerSize) return NULL;
+	if(len < headerSize) return false;
 
-	Message* msg = (Message*)buf;
+	// 复制头4字节
+	*(uint*)this = *(uint*)buf;
+	// 复制校验和
+	Checksum = *(ushort*)(buf + 4);
+
 	if(len > headerSize)
 	{
-		msg->Length = len - headerSize;
-		msg->Data = buf + headerSize;
+		Length = len - headerSize;
+		Data = buf + headerSize;
+	}
+	else
+	{
+		Length = 0;
+		Data = NULL;
 	}
 
-	return msg;
+	return true;
 }
 
 // 验证消息校验和是否有效
-bool Verify()
+bool Message::Verify()
 {
-	if(!data) len == 0;
-
-	return Length == len;
+	return false;
 }
 
 // 构造控制器
@@ -45,14 +52,138 @@ Controller::Controller(ITransport* port)
 	_port = port;
 }
 
-void Controller::OnReceive(ITransport* transport, byte* buf, uint len, void* param)
+Controller::~Controller()
+{
+	int count = _Handlers.Count();
+	for(int i=0; i<count; i++)
+	{
+		if(_Handlers[i]) delete _Handlers[i];
+	}
+}
+
+uint Controller::OnReceive(ITransport* transport, byte* buf, uint len, void* param)
 {
 	assert_ptr(param);
 
 	Controller* control = (Controller*)param;
-	control->Process(byte* buf, uint len);
+	control->Process(buf, len);
+
+	return 0;
 }
 
 void Controller::Process(byte* buf, uint len)
 {
+	Message msg;
+	if(!msg.Parse(buf, len) || !msg.Verify()) return;
+
+	int count = _Handlers.Count();
+	for(int i=0; i<count; i++)
+	{
+		CommandHandlerLookup* lookup = _Handlers[i];
+		if(lookup && lookup->Code == msg.Code)
+		{
+			// 如果处理成功则退出，否则继续遍历其它处理器
+			if(lookup->Handler(msg)) return;
+		}
+	}
+}
+
+void Controller::Register(byte code, CommandHandler handler)
+{
+	assert_param(code);
+	assert_param(handler);
+
+	CommandHandlerLookup* lookup = new CommandHandlerLookup();
+	lookup->Code = code;
+	lookup->Handler = handler;
+
+	_Handlers.Add(lookup);
+}
+
+uint Controller::Send(byte dest, byte code, byte* buf, uint len)
+{
+	Message msg;
+	msg.Init();
+	msg.Dest = dest;
+	msg.Code = code;
+	msg.Length = len;
+	msg.Data = buf;
+
+	return Send(msg);
+}
+
+bool Controller::Send(Message& msg, uint msTimeout)
+{
+	// 附上自己的地址
+	msg.Src = Address;
+
+	// 先发送头部，然后发送负载数据
+	byte* buf = (byte*)&msg;
+	_port->Write(buf, 6);
+	if(msg.Length > 0)
+		_port->Write(msg.Data, msg.Length);
+
+	/*// 等待响应
+	TimeWheel tw(0, msTimeout, 0);
+	while(true)
+	{
+		if(tw.Expired) return false;
+
+		xxx
+	}*/
+
+	return true;
+}
+
+void Controller::Reply(Message& msg)
+{
+	msg.Reply = 1;
+	Send(msg);
+}
+
+bool Controller_Test_Discover(const Message& msg)
+{
+	if(!msg.Reply)
+	{
+		debug_printf("收到来自[%d]的Discover请求！", msg.Src);
+		if(msg.Length > 0)
+		{
+			debug_printf("数据：[%d] ", msg.Length);
+			Sys.ShowString(msg.Data, msg.Length);
+		}
+		debug_printf("\r\n");
+	}
+	else if(!msg.Error)
+	{
+		debug_printf("收到来自[%d]的Discover响应！", msg.Src);
+		if(msg.Length > 0)
+		{
+			debug_printf("数据：[%d] ", msg.Length);
+			Sys.ShowString(msg.Data, msg.Length);
+		}
+		debug_printf("\r\n");
+	}
+	else
+	{
+		debug_printf("收到来自[%d]的Discover错误！", msg.Src);
+		if(msg.Length > 0)
+		{
+			debug_printf("数据：[%d] ", msg.Length);
+			Sys.ShowString(msg.Data, msg.Length);
+		}
+		debug_printf("\r\n");
+	}
+
+	return true;
+}
+
+void Controller::Test(ITransport* port)
+{
+	Controller control(port);
+	control.Address = 2;
+
+	const byte DISC_CODE = 1;
+	control.Register(DISC_CODE, Controller_Test_Discover);
+
+	control.Send(3, DISC_CODE);
 }
--- TinyNet/Message.h
+++ TinyNet/Message.h
@@ -2,13 +2,13 @@
 #define __Message_H__
 
 #include "Sys.h"
-#include "ITransport.h"
+#include "Net\ITransport.h"
 
 // 消息
 // 头部按照内存布局，但是数据和校验部分不是
 class Message
 {
-private:
+public:
 	// 标准头部，符合内存布局。注意凑够4字节，否则会有很头疼的对齐问题
 	byte Dest;		// 目的地址
 	byte Src;		// 源地址
@@ -26,12 +26,9 @@ public:
 	// 初始化消息，各字段为0
 	void Init();
 	// 分析数据，转为消息。负载数据部分将指向数据区，外部不要提前释放内存
-	void Parse(byte* buf, uint len);
+	bool Parse(byte* buf, uint len);
 	// 验证消息校验和是否有效
 	bool Verify();
-	
-public:	// 静态部分
-	//static 
 };
 
 class Controller
@@ -39,19 +36,47 @@ class Controller
 private:
 	ITransport* _port;	// 数据传输口
 
-	void OnReceive(ITransport* transport, byte* buf, uint len, void* param);
+	static uint OnReceive(ITransport* transport, byte* buf, uint len, void* param);
 
 public:
-	Controller(ITransport* port);
+	byte Address;	// 本地地址
 	
+	Controller(ITransport* port);
+	~Controller();
+
+	uint Send(byte dest, byte code, byte* buf = NULL, uint len = 0);
+	bool Send(Message& msg, uint msTimeout = 10);
+	void Reply(Message& msg);
+
 protected:
 	void Process(byte* buf, uint len);
-}
 
-class DiscoverMessage : public Message
+// 处理器部分
+public:
+	// 处理消息，返回是否成功
+	typedef bool (*CommandHandler)(const Message& msg);
+
+private:
+    struct CommandHandlerLookup
+    {
+        uint Code;
+        CommandHandler Handler;
+    };
+	List<CommandHandlerLookup*> _Handlers;
+
+public:
+	// 注册消息处理器。考虑到业务情况，不需要取消注册
+	void Register(byte code, CommandHandler handler);
+
+// 测试部分
+public:
+	static void Test(ITransport* port);
+};
+
+/*class DiscoverMessage : public Message
 {
 public:
-	//const 
-}
+	//const
+}*/
 
 #endif
