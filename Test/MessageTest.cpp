7689e9e9cdb20efb2821a6c582e9e42a404aa18f
Stone <nnhy@vip.qq.com>
2014-11-07 05:16:39 +0000

    0x28通道，1Mbps速度

--- Test/MessageTest.cpp
+++ Test/MessageTest.cpp
@@ -73,10 +73,12 @@ void TestMessage(OutputPort* leds)
 
 	NRF24L01* nrf = Create2401();
     //nrf->Timeout = 1000;
-    nrf->Channel = 120;
+    nrf->Channel = 0x28;
 	// 如果打开自动应答，繁忙时收不到数据会增加错误计数，达到一定程度会自动重启模块
     nrf->AutoAnswer = false;
-	nrf->Speed = 250;
+	nrf->Speed = 1000;
+	byte addr[] = {0x34, 0x43, 0x10, 0x10, 0x01};
+	memcpy(nrf->Address, addr, ArrayLength(addr));
     if(!nrf->Check())
         debug_printf("请检查NRF24L01线路\r\n");
 
