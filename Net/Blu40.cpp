e2ac2bb33f419749634c813deffe57cc19e0ec40
WangQiang <807319473@qq.com>
2015-05-23 10:43:53 +0000

    编译通过  未测试

--- Net/Blu40.cpp
+++ Net/Blu40.cpp
@@ -55,28 +55,41 @@ X:
 const byte AT_TPL[] = "AT:TPL-";
 
 const byte ATOK[] = "AT:OK\r\n\0";
-const byte ATERR[] = "AT:ERR\r\n\0";
+//const byte ATERR[] = "AT:ERR\r\n\0";
 
+
+const int TPLNum[] = {-23,-6,0,4};
+	
 Blu40::Blu40()
 {
 	_rts = NULL;
 	_cts = NULL;
+	_baudRate = 0;
 }
 
-Blu40::Blu40(ITransport *port,Pin rts = P0 ,Pin cts = P0, OutputPort * rst = NULL)
+Blu40::Blu40(SerialPort *port,Pin rts ,Pin cts, OutputPort * rst )
 {
 	assert_ptr(port);
+	_baudRate = 0;
 	Init(port,rts,cts,rst);
 }
 
-void Blu40::Init(ITransport *port = NULL,Pin rts = P0,Pin cts = P0,OutputPort * rst = NULL)
+void Blu40::Init(SerialPort *port ,Pin rts,Pin cts,OutputPort * rst)
 {
 	if(!port) _port = port;
 	if(rts != P0)_rts = new OutputPort(rts);
 	//if(cts != P0)_cts = new InputPort(cts);
 	//_cts.Register();
 	if(!rst)_rst = rst;
-	Reset();
+	Reset();	
+}
+
+Blu40::~Blu40()
+{
+	if(_port)delete _port;
+	if(_rts)delete _rts;
+	if(_cts)delete _cts;
+	if(_rst)delete _rst;
 }
 
 void Blu40::Reset()
@@ -84,38 +97,75 @@ void Blu40::Reset()
 	// RESET ENABLE OF LOWER
 	if(!_rst)return;
 	*_rst = false;
-	for(int i = 0;i<100;i++);
+	Sys.Delay(100);
 	*_rst = true;
 }
 
-bool Blu40::SetBP(uint BP)
+bool Blu40::SetBP(int BP)
 {
 	if(BP>115200)
 	{
-		debug_prinf("Blu不支持如此高的波特率");
+		debug_printf("Blu不支持如此高的波特率");
 	}
 	//const byte AT_BPSNum[] = {'0','1','2','3','4','5','6','7'};
 	byte bpnumIndex;
-	for( bpnumIndex =0,_bp=1200;_bp == BP;_bp<<=1,bpnumIndex++ );
-	
-	_port->Write(AT_BPS,sizof(AT_BPS));
-	//_port->Write(AT_BPSNum[bpnumIndex],1);
-	_port->Write(bpnumIndex+'0',1);
+	int _bp;
+	for( bpnumIndex =0, _bp=1200;_bp == BP;_bp*=2,bpnumIndex++ );
 	
 	byte buf[40];
-	_port->Read(buf,40);
-	//"AT:BPS SET AFTER 2S \r\n\0"
-	//"AT:ERR\r\n\0"
-	const byte BPSOK[] = "AT:BPS SET AFTER 2S \r\n\0";
-	for(int i=0,void *p = buf,void *p2 = BPSOK;i<sizeof(BPSOK);i++)
+	byte BPSOK[] = "AT:BPS SET AFTER 2S \r\n\0";
+	if(_baudRate != 0)
 	{
-		if(*p != *p2)
+		_port->SetBaudRate(_baudRate);
+		_port->Write(AT_BPS,sizeof(AT_BPS));
+		//_port->Write(AT_BPSNum[bpnumIndex],1);
+		byte temp = bpnumIndex+'0';
+		_port->Write(&temp,1);
+		_port->Read(buf,40);
+
+		for(int j=0;j<sizeof(BPSOK);j++)
 		{
-			debug_prinf("设置失败，请检查现在使用的波特率是否正确");
-			return false;
+			if(buf[j] != BPSOK[j])
+			{
+				debug_printf("设置失败，重新调整波特率进行设置");
+				break;
+			}
 		}
+		debug_printf("设置成功，2S后启用新波特率\r\n");
+		return true;
 	}
-	debug_prinf("设置成功，2S后启用新波特率\r\n");
+	{
+		int portBaudRateTemp = 115200;
+		for(int i = 0;i<7;i++)
+		{
+			_port->SetBaudRate(	portBaudRateTemp);
+			_port->Write(AT_BPS,sizeof(AT_BPS));
+			//_port->Write(AT_BPSNum[bpnumIndex],1);
+			byte temp = bpnumIndex+'0';
+			_port->Write(&temp,1);
+			
+			_port->Read(buf,40);
+			//"AT:BPS SET AFTER 2S \r\n\0"
+			//"AT:ERR\r\n\0"
+			for(int j=0;j<sizeof(BPSOK);j++)
+			{
+				if(buf[j] != BPSOK[j])
+				{
+					if(i==7)
+					{	
+						debug_printf("设置失败，请检查现在使用的波特率是否正确");
+						_baudRate = 0;
+						return false;
+					}
+					debug_printf("设置失败，重新调整当前波特率进行设置");
+					portBaudRateTemp/=2;
+					break;
+				}
+			}
+		}
+	_baudRate = portBaudRateTemp;
+	}	
+	debug_printf("设置成功，2S后启用新波特率\r\n");
 	return true;
 }
 
@@ -123,29 +173,29 @@ bool Blu40::CheckSet()
 {
 	byte buf[40];
 	_port->Read(buf,40);
-	for(int i=0,void *p = buf,void *p2 = ATOK;i<sizeof(ATOK);i++)
+	for(int i=0;i<sizeof(ATOK);i++)
 	{
-		if(*p != *p2)
+		if(buf[i] != ATOK[i])
 		{
-			debug_prinf("设置失败");
+			debug_printf("设置失败");
 			return false;
 		}
 	}
-	debug_prinf("设置成功\r\n");
+	debug_printf("设置成功\r\n");
 	return true;
 }
 
 bool Blu40::SetName(string name)
 {
-	_port->Write(AT_REN,sizof(AT_REN));
-	_port->Write(name,sizeof(name));
+	_port->Write(AT_REN,sizeof(AT_REN));
+	_port->Write((byte*)name,sizeof(name));
 	return CheckSet();
 }
 
 bool Blu40::SetPID(ushort pid)
 {
-	_port->Write(AT_PID,sizof(AT_REN));
-	_port->Write(&pid,2);
+	_port->Write(AT_PID,sizeof(AT_REN));
+	_port->Write((byte *)pid,2);
 	return CheckSet();
 }
 
@@ -153,11 +203,31 @@ bool Blu40::SetPID(ushort pid)
 bool Blu40::SetTPL(int TPLDB)
 {
 	byte TPLNumIndex ;
-	for(TPLNumIndex= 0;TPLNumIndex<sizof(TPLNum);TPLNumIndex++)
+	for(TPLNumIndex= 0;TPLNumIndex<sizeof(TPLNum);TPLNumIndex++)
 	{
 		if(TPLNum[TPLNumIndex]==TPLDB)break;
 	}
-	_port->Write(AT_TPL,sizof(AT_TPL));
-	_port->Write(TPLNumIndex+'0',1);
+	_port->Write(AT_TPL,sizeof(AT_TPL));
+	byte temp = TPLNumIndex+'0';
+	_port->Write(&temp,1);
 	return CheckSet();
+}
+
+// 注册回调函数
+void Blu40::Register(TransportHandler handler, void* param)
+{
+	ITransport::Register(handler, param);
+
+	if(handler)
+		_port->Register(OnPortReceive, this);
+	else
+		_port->Register(NULL);
+}
+
+uint Blu40::OnPortReceive(ITransport* sender, byte* buf, uint len, void* param)
+{
+	assert_ptr(param);
+
+	Blu40* blu = (Blu40*)param;
+	return blu->OnReceive(buf, len);
 }
--- Net/Blu40.h
+++ Net/Blu40.h
@@ -1,28 +1,42 @@
 ﻿
-#ifndef __Zigbee_H__
-#define __Zigbee_H__
+#ifndef __BLU40_H__
+#define __BLU40_H__
 
 #include "Sys.h"
 #include "Port.h"
 #include "Net\ITransport.h"
+#include "SerialPort.h"
 
 // 司卡乐 CC2540
 class Blu40 : public ITransport
 {
 private:
-	ITransport *_port;
+	SerialPort *_port;
 	OutputPort *_rts;
 	InputPort *_cts;
 	OutputPort *_rst;
+	int _baudRate;
 public:
 	Blu40();
-	Blu40(ITransport *port = NULL,Pin rts = P0 ,Pin cts = P0, OutputPort * rst = NULL);
+	Blu40(SerialPort *port,Pin rts = P0 ,Pin cts = P0, OutputPort * rst = NULL);
 	virtual ~Blu40();
-	void Init(ITransport *port = NULL,Pin rts = P0,Pin cts = P0,OutputPort * rst = NULL);
+	void Init(SerialPort *port = NULL,Pin rts = P0,Pin cts = P0,OutputPort * rst = NULL);
 	
 	virtual void Register(TransportHandler handler, void* param = NULL);
 	virtual void Reset(void);
+
+	// 设置波特路
+	bool SetBP(int BP);
+	// 检查设置是否成功 使用大部分指令
+	bool CheckSet();
+	// 设置发送信号强度 DB数
+	bool SetTPL(int TPLDB);
+	// 设置蓝牙名称
+	bool SetName(string name);
+	// 设置产品识别码 硬件类型code
+	bool SetPID(ushort pid);
 	
+	virtual string ToString() { return "Blutooth4.0"; }
 protected:
 	virtual bool OnOpen() { return _port->Open(); }
     virtual void OnClose() { _port->Close(); }
@@ -31,16 +45,5 @@ protected:
 	virtual uint OnRead(byte* buf, uint len) { return _port->Read(buf, len); }
 
 	static uint OnPortReceive(ITransport* sender, byte* buf, uint len, void* param);
-	
-	// 设置波特路
-	bool SetBP(uint BP);
-	// 检查设置是否成功 使用大部分指令
-	bool CheckSet();
-	// 设置发送信号强度 DB数
-	const int TPLNum[] = {-23,-6,0,4};
-	bool SetTPL(int TPLDB);
-	// 设置蓝牙名称
-	bool SetName(string name);
-	// 设置产品识别码 硬件类型code
-	bool SetPID(ushort pid);
 };
+#endif
