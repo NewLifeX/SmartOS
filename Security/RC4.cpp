258eb17a9fb3242304b8f91ef28a3204609e0618
nnhy <nnhy@vip.qq.com>
2015-06-15 03:52:07 +0000

    增加加解密算法目录，增加RC4算法，未测试

---
+++ Security/RC4.cpp
@@ -0,0 +1,41 @@
+﻿#include "RC4.h"
+
+void GetKey(byte* box, byte* pass, Int32 len)
+{
+	for (int i = 0; i < len; i++)
+	{
+		box[i] = i;
+	}
+	int j = 0;
+	for (int i = 0; i < len; i++)
+	{
+		j = (j + box[i] + pass[i % pass.Length]) % len;
+		var temp = box[i];
+		box[i] = box[j];
+		box[j] = temp;
+	}
+	return box;
+}
+
+void RC4::Encrypt(byte* data, uint len, byte* pass, uint plen)
+{
+	assert_ptr(data);
+	assert_ptr(pass);
+
+	int i = 0;
+	int j = 0;
+	byte box[256];
+	GetKey(box, pass, ArrayLength(box));
+	// 加密  
+	for (int k = 0; k < data.Length; k++)
+	{
+		i = (i + 1) % box.Length;
+		j = (j + box[i]) % box.Length;
+		byte temp = box[i];
+		box[i] = box[j];
+		box[j] = temp;
+		byte a = data[k];
+		byte b = box[(box[i] + box[j]) % box.Length];
+		data[k] = (byte)(a ^ b);
+	}
+}
---
+++ Security/RC4.h
@@ -0,0 +1,16 @@
+﻿#ifndef __RC4_H__
+#define __RC4_H__
+
+#include "Sys.h"
+
+// RC4 加密算法
+class RC4
+{
+public:
+	static int KeyLength = 256;
+
+	// 加解密
+	static void Encrypt(byte* data, uint len, byte* pass, uint plen);
+};
+
+#endif
