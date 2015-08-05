6717ac44f61d0e237f27619ac9d066948cb55164
nnhy <nnhy@vip.qq.com>
2015-08-05 04:40:42 +0000

    增加字节环形队列，默认64字节大小。编译通过，未测试。 准备首先在串口驱动上使用

---
+++ Queue.cpp
@@ -0,0 +1,75 @@
+﻿#include "Queue.h"
+
+Queue::Queue(uint len)
+{
+	if(len <= ArrayLength(_Arr))
+	{
+		len = ArrayLength(_Arr);
+		_Buffer = _Arr;
+		_needFree = false;
+	}
+	else
+	{
+		_Buffer = new byte[len];
+		_needFree = true;
+	}
+
+	_Capacity	= len;
+	_size		= 0;
+	_head		= 0;
+	_tail		= 0;
+}
+
+// 使用缓冲区初始化缓冲区。注意，此时指针位于0，而内容长度为缓冲区长度
+Queue::Queue(byte* buf, uint len)
+{
+	assert_ptr(buf);
+	//assert_param2(len > 0, "不能用0长度缓冲区来初始化缓冲区");
+
+	_Buffer		= buf;
+	_Capacity	= len;
+	_size		= 0;
+	_head		= 0;
+	_tail		= 0;
+}
+
+// 销毁缓冲区
+Queue::~Queue()
+{
+	assert_ptr(this);
+	if(_needFree)
+	{
+		if(_Buffer != _Arr) delete[] _Buffer;
+		_Buffer = NULL;
+	}
+}
+
+void Queue::Clear()
+{
+	_size		= 0;
+	_head		= 0;
+	_tail		= 0;
+}
+
+void Queue::Push(byte dat)
+{
+	_Buffer[_head++] = dat;
+	_head %= _Capacity;
+	_size++;
+}
+
+byte Queue::Pop()
+{
+	if(_size == 0) return 0;
+
+	byte dat = _Buffer[_tail++];
+	_tail %= _Capacity;
+	_size--;
+
+	return dat;
+}
+
+byte Queue::Peek() const
+{
+	return _Buffer[_tail];
+}
---
+++ Queue.h
@@ -0,0 +1,42 @@
+﻿#ifndef _Queue_H_
+#define _Queue_H_
+
+#include <stddef.h>
+#include "Sys.h"
+
+// 内存缓冲区
+// 缓冲区内有一个缓冲区，游标位置，数据长度。实际有效数据仅占用缓冲区中间部分，头尾都可能有剩余
+class Queue
+{
+private:
+	byte* _Buffer;	// 数据缓冲区。扩容后会重新分配缓冲区
+	uint _Capacity;	// 缓冲区容量
+	uint _size;		// 大小
+	uint _head;		// 头部位置
+    uint _tail;		// 尾部位置
+	// 又是头疼的对齐问题
+	uint _needFree;	// 是否自动释放
+
+	byte _Arr[64];	// 内部缓冲区。较小内存需要时，直接使用栈分配，提高性能。
+public:
+
+	// 分配指定大小的缓冲区
+	Queue(uint len = 0);
+	// 使用缓冲区初始化缓冲区。注意，此时指针位于0，而内容长度为缓冲区长度
+	Queue(byte* buf, uint len);
+	// 销毁缓冲区
+	~Queue();
+
+	bool Full() const { return _size == _Capacity; }// 队列满
+	bool Empty() const { return _size == 0; }		// 队列空
+	uint Capacity() const { return _Capacity; }		// 队列容量
+	uint Length() const { return _size; }			// 队列大小
+
+	void Clear();
+	
+	void Push(byte dat);
+	byte Pop();
+	byte Peek() const;
+};
+
+#endif
