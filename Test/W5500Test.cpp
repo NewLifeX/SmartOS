7467a4be62ed83691550ed98b2525a0a308e679f
WangQiang <807319473@qq.com>
2015-08-17 01:40:37 +0000

    增加UdpClient 发送测试， 实际运行失败

--- Test/W5500Test.cpp
+++ Test/W5500Test.cpp
@@ -3,7 +3,21 @@
 void TestTask(void* param)
 {
     W5500* net = (W5500*)param;
-	if(net->CheckLink()) net->PhyStateShow();
+	//if(net->CheckLink()) net->PhyStateShow();
+	static bool netStaChang = true;
+	if(netStaChang != net->CheckLink())
+	{
+		net->PhyStateShow();
+		netStaChang = !netStaChang;
+	}
+}
+
+void TestTask2(void* param)
+{
+	UdpClient* udp = (UdpClient*)param;
+	debug_printf("尝试发送udp数据\r\n");
+	byte buf[]  = "hello";
+	udp->Write(buf,sizeof(buf));
 }
 
 void TestW5500(Spi* spi, Pin irq, OutputPort* reset)
@@ -12,12 +26,16 @@ void TestW5500(Spi* spi, Pin irq, OutputPort* reset)
 	net->Init(spi, irq, reset);
 
 	net->IP = IPAddress(192, 168, 0, 200);
-
-	if(net->CheckLink()) debug_printf("OK");
-	net->StateShow();
-	
-	UdpClient udp(net);
-	udp.Open();
-
+	net->Gateway = IPAddress(192,168,0,1);
+	// 定时检查网络状况
 	Sys.AddTask(TestTask, net, 10000000, 10000000, "TestW5500");
+	net->StateShow();
+	net->Open();
+	
+	UdpClient *udp = new UdpClient(net);
+	IPAddress RemoteIP(192,168,0,16);
+	udp->Remote = IPEndPoint(RemoteIP,3377);
+	udp->Open();
+	Sys.AddTask(TestTask2, udp, 9000000, 10000000, "TestUdpClient");
+	
 }
