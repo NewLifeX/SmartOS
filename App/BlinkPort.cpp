a0b46878f3a6a714b7ec3346fe5d2af97b868a15
nnhy <nnhy@vip.qq.com>
2015-08-18 11:04:25 +0000

    增加闪烁端口应用类，无阻塞，便于无阻塞闪烁小灯和蜂鸣器，在门磁上测试通过

---
+++ App/BlinkPort.cpp
@@ -0,0 +1,67 @@
+#include "BlinkPort.h"
+#include "Task.h"
+
+void BlinkTask(void* param);
+
+BlinkPort::BlinkPort()
+{
+	ArrayZero(Ports);
+	Count	= 0;
+
+	First	= true;
+	Times	= 10;
+	Interval1	= 100000;
+	Interval2	= 300000;
+	Index		= 0;
+
+	_tid	= Sys.AddTask(BlinkTask, this, -1, -1, "闪烁端口");
+}
+
+BlinkPort::~BlinkPort()
+{
+	if(_tid) Sys.RemoveTask(_tid);
+}
+
+void BlinkPort::Add(OutputPort* port)
+{
+	Ports[Count++] = port;
+}
+
+void BlinkPort::Start()
+{
+	Current	= First;
+	Index	= 0;
+
+	// 开启一次任务
+	Sys.SetTask(_tid, true);
+}
+
+void BlinkPort::Blink()
+{
+	bool f = Current;
+	int t = Interval1;
+	if(Index & 0x01) t = Interval2;
+
+	//debug_printf("翻转到状态 %d Index=%d Next=%d\r\n", Current, Index, t);
+
+	for(int i=0; i<Count; i++)
+	{
+		Ports[i]->Write(f);
+	}
+
+	// 如果闪烁次数不够，则等下一次
+	Index++;
+	if(Index < Times)
+	{
+		Current = !Current;
+
+		// 切换任务执行时间
+		Sys.SetTask(_tid, true, t);
+	}
+}
+
+void BlinkTask(void* param)
+{
+	BlinkPort* bp = (BlinkPort*)param;
+	bp->Blink();
+}
---
+++ App/BlinkPort.h
@@ -0,0 +1,33 @@
+﻿#ifndef __BlinkPort_H__
+#define __BlinkPort_H__
+
+#include "Sys.h"
+#include "Port.h"
+
+// 闪烁端口
+class BlinkPort
+{
+private:
+	int		_tid;
+
+public:
+	OutputPort*	Ports[0x10];
+	int		Count;
+
+	bool	First;		// 开始状态
+	int		Times;		// 变更次数。高低算两次
+	int		Interval1;	// 间隔1。第一次改变状态后暂停时间，默认100000us
+	int		Interval2;	// 间隔2。第二次改变状态后暂停时间，默认300000us
+
+	bool 	Current;	// 当前值
+	int		Index;		// 当前闪烁次数
+	
+	BlinkPort();
+	~BlinkPort();
+
+	void Add(OutputPort* port);
+	void Start();
+	void Blink();
+};
+
+#endif
