6703873a99db576659f81aaea7ba092e18624c3b
WangQiang <807319473@qq.com>
2015-09-05 09:08:11 +0000

    回滚之前udp粘包处理  Port类编译不通过 无法测试

--- Drivers/W5500.cpp
+++ Drivers/W5500.cpp
@@ -1097,6 +1097,8 @@ void HardSocket::ClearRX()
 	ushort offset = __REV16(SocRegRead2(RX_RD));
 	// 读指针直接 = 接收指针，即让数据区无效
 	SocRegWrite2(RX_RD, __REV16(offset + size));
+	// 生效 RX_RD
+	WriteConfig(RECV);
 }
 
 void HardSocket::Recovery()
@@ -1327,71 +1329,99 @@ void UdpClient::OnProcess(byte reg)
 
 void UdpClient::Receive()
 {
-	byte packetHead[8];	// 数据头
-	byte dataBuf[1472];	// 最大udp单元大小
-	while(true)
+	// UDP 异步只有一种情况  收到数据  可能有多个数据包
+	// UDP接收到的数据结构： RemoteIP(4 byte) + RemotePort(2 byte) + Length(2 byte) + Data(Length byte)
+	byte buf[1024];
+	ByteArray bs(buf, ArrayLength(buf));
+	ushort size = ReadByteArray(bs);
+	Stream ms(bs.GetBuffer(), size);
+
+	// 拆包
+	while(ms.Remain())
 	{
-		if(DataLength == 0)
+		ByteArray bs2(6);
+		ms.Read(bs2);
+
+		ushort len = ms.Read<ushort>();
+		len = __REV16(len);
+		// 数据长度不对可能是数据错位引起的，直接丢弃数据包
+		if(len > 1500)
 		{
-			// 读取包头
-			ByteArray bsHead(packetHead, ArrayLength(packetHead));
-			ushort size = ReadByteArray(bsHead, true);
-			if(size == ArrayLength(packetHead))
-			{
-				// 解析头
-				Stream ms(bsHead.GetBuffer(), size);
-				ByteArray bs2(6);
-				ms.Read(bs2);
-
-				ushort len = ms.Read<ushort>();
-				len = __REV16(len);
-
-				// debug_printf("W5500 UDP,Length: %d 	",len);
-
-				// 数据长度不对可能是数据错位引起的，直接丢弃数据包
-				if(len > 1472)
-				{
-					debug_printf("W5500 UDP数据接收有误,Length: %d \r\n",len);
-					// 丢弃所有数据
-					ClearRX();
-					DataLength = 0;
-					return;
-				}
-				// 确认数据长度在合理范围才将正确 Endpoint 记录下来
-				IPEndPoint ep(bs2);
-				ep.Port = __REV16(ep.Port);
-				DataLength = len;
-			}
-			else if(size == 0)
-			{
-				DataLength = 0;
-				return ;
-			}
-			else
-			{	// 没有读出完整头部  直接丢弃所有数据
-				ClearRX();
-				DataLength = 0;
-				return ;
-			}
+			debug_printf("W5500 UDP数据接收有误,Length: %d \r\n",len);
+			return;
 		}
-		else	// 根据数据长度读取数据并处理
-		{
-			// 读取当前RX数据区内剩余量
-			ushort RemainLength = __REV16(SocRegRead2(RX_RSR));
-			// W5500 未接收数据不足包 放弃不读
-			if(RemainLength < DataLength) return;
-			// 读取数据
-			ByteArray bsData(dataBuf, DataLength);
-			ushort datasize = ReadByteArray(bsData, true);
+		// 确认数据长度在合理范围才将正确 Endpoint 记录下来
+		IPEndPoint ep(bs2);
+		ep.Port = __REV16(ep.Port);
+		// 回调中断
+		OnReceive(ms.ReadBytes(len), len);
+	};
 
-			// debug_printf("Data: ");
-			// bsData.Show();
-			// debug_printf("\r\n");
-			Stream ms(bsData.GetBuffer(), datasize);
-			// 数据向上层发送
-			OnReceive(ms.ReadBytes(DataLength), DataLength);
-			// 这个数据包的数据都拿到了并处理了
-			DataLength = 0;
-		}
-	}
+	//byte packetHead[8];	// 数据头
+	//byte dataBuf[1472];	// 最大udp单元大小
+	//while(true)
+	//{
+	//	if(DataLength == 0)
+	//	{
+	//		// 读取包头
+	//		ByteArray bsHead(packetHead, ArrayLength(packetHead));
+	//		ushort size = ReadByteArray(bsHead, true);
+	//		if(size == ArrayLength(packetHead))
+	//		{
+	//			// 解析头
+	//			Stream ms(bsHead.GetBuffer(), size);
+	//			ByteArray bs2(6);
+	//			ms.Read(bs2);
+    //
+	//			ushort len = ms.Read<ushort>();
+	//			len = __REV16(len);
+    //
+	//			// debug_printf("W5500 UDP,Length: %d 	",len);
+    //
+	//			// 数据长度不对可能是数据错位引起的，直接丢弃数据包
+	//			if(len > 1472)
+	//			{
+	//				debug_printf("W5500 UDP数据接收有误,Length: %d \r\n",len);
+	//				// 丢弃所有数据
+	//				ClearRX();
+	//				DataLength = 0;
+	//				return;
+	//			}
+	//			// 确认数据长度在合理范围才将正确 Endpoint 记录下来
+	//			IPEndPoint ep(bs2);
+	//			ep.Port = __REV16(ep.Port);
+	//			DataLength = len;
+	//		}
+	//		else if(size == 0)
+	//		{
+	//			DataLength = 0;
+	//			return ;
+	//		}
+	//		else
+	//		{	// 没有读出完整头部  直接丢弃所有数据
+	//			ClearRX();
+	//			DataLength = 0;
+	//			return ;
+	//		}
+	//	}
+	//	else	// 根据数据长度读取数据并处理
+	//	{
+	//		// 读取当前RX数据区内剩余量
+	//		ushort RemainLength = __REV16(SocRegRead2(RX_RSR));
+	//		// W5500 未接收数据不足包 放弃不读
+	//		if(RemainLength < DataLength) return;
+	//		// 读取数据
+	//		ByteArray bsData(dataBuf, DataLength);
+	//		ushort datasize = ReadByteArray(bsData, true);
+    //
+	//		// debug_printf("Data: ");
+	//		// bsData.Show();
+	//		// debug_printf("\r\n");
+	//		Stream ms(bsData.GetBuffer(), datasize);
+	//		// 数据向上层发送
+	//		OnReceive(ms.ReadBytes(DataLength), DataLength);
+	//		// 这个数据包的数据都拿到了并处理了
+	//		DataLength = 0;
+	//	}
+	//}
 }
