46a53b0026770da66055d6b2af67584d4ecd1682
nnhy <nnhy@vip.qq.com>
2015-09-19 02:46:45 +0000

    命令端口。通过命令来控制输出口开关、取反、延时打开、打开延迟等高级功能 编译通过，未测试

---
+++ App/CommandPort.cpp
@@ -0,0 +1,91 @@
+﻿#include "CommandPort.h"
+#include "Task.h"
+
+static void CommandTask(void* param);
+
+CommandPort::CommandPort(OutputPort* port, byte* data)
+{
+	_tid	= 0;
+
+	Port	= port;
+	Data	= data;
+}
+
+CommandPort::~CommandPort()
+{
+	Sys.RemoveTask(_tid);
+}
+
+void CommandPort::Set(byte cmd)
+{
+	if(!Port) return;
+
+	OutputPort& port = *Port;
+	switch(cmd)
+	{
+		case 1:
+			debug_printf("打开");
+			port = true;
+			break;
+		case 0:
+			debug_printf("关闭");
+			port = false;
+			break;
+		case 2:
+			debug_printf("反转");
+			port = !port;
+			break;
+		default:
+			debug_printf("控制0x%02X", cmd);
+			return;
+	}
+	switch(cmd>>4)
+	{
+		// 普通指令
+		case 0:
+			break;
+		// 打开，延迟一段时间后关闭
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+			port = true;
+			Next = 0;
+			StartAsync(cmd - 0x10);
+			break;
+		// 关闭，延迟一段时间后打开
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+			port = false;
+			Next = 1;
+			StartAsync(cmd - 0x50);
+			break;
+	}
+#if DEBUG
+	Name.Show(true);
+#endif
+}
+
+void CommandPort::Set()
+{
+	if(!Data) return;
+
+	Set(*Data);
+
+	if(Port) *Data = Port->Read() ? 1 : 0;
+}
+
+static void CommandTask(void* param)
+{
+	CommandPort* port = (CommandPort*)param;
+	port->Set(port->Next);
+}
+
+void CommandPort::StartAsync(int second)
+{
+	if(!_tid) _tid = Sys.AddTask(CommandTask, this, -1, -1, "命令端口");
+	//Sys.SetTaskPeriod(second * 1000000);
+	Sys.SetTask(_tid, true, second * 1000000);
+}
---
+++ App/CommandPort.h
@@ -0,0 +1,30 @@
+﻿#ifndef __CommandPort_H__
+#define __CommandPort_H__
+
+#include "Sys.h"
+#include "Port.h"
+
+// 命令端口。通过命令来控制输出口开关、取反、延时打开、打开延迟等高级功能
+class CommandPort
+{
+private:
+	uint	_tid;
+
+public:
+	OutputPort*	Port;	// 输出口
+	byte*		Data;	// 绑定的数据位
+	byte		Next;	// 定时执行的下一条命令
+#if DEBUG
+	String		Name;	// 名称
+#endif
+
+	CommandPort(OutputPort* port, byte* data = NULL);
+	~CommandPort();
+
+	void Set(byte cmd);	// 根据命令执行动作
+	void Set();			// 根据绑定的数据位执行动作
+	
+	void StartAsync(int second);
+};
+
+#endif
