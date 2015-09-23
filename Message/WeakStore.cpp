a6e7aa33ca69477aa49265bdad78001c6f224d8d
nnhy <nnhy@vip.qq.com>
2015-09-23 08:32:04 +0000

    增加弱存储，采用顶端栈分配的空间，确保数据不会被初始化，即使重启只要不掉电也保持数据

---
+++ Message/WeakStore.cpp
@@ -0,0 +1,34 @@
+﻿#include "WeakStore.h"
+
+// 初始化
+WeakStore::WeakStore(int magic, byte* ptr, uint len) : Data(0x40)
+{
+	Magic	= magic;
+
+	// 如果指定了外部空间，则使用外部空间，否则使用内部默认分配的空间
+	if(ptr && len > 0) Data.Set(ptr, len);
+
+	if(magic && Check())
+	{
+		debug_printf("首次上电  ");
+		Init();
+	}
+}
+
+// 检查并确保初始化，返回原来是否已初始化
+bool WeakStore::Check()
+{
+	int mg = *(int*)Data.GetBuffer();
+	return mg == Magic;
+}
+
+void WeakStore::Init()
+{
+	debug_printf("初始化 0x%08X，幻数 0x%08X\r\n", Data.GetBuffer(), Magic);
+	Data.Copy((byte*)&Magic, 4);
+}
+
+byte& WeakStore::operator[](int i) const
+{
+	return Data[i];
+}
---
+++ Message/WeakStore.h
@@ -0,0 +1,25 @@
+﻿#ifndef __WeakStore_H__
+#define __WeakStore_H__
+
+#include "Sys.h"
+#include "Stream.h"
+
+// 弱存储
+class WeakStore
+{
+public:
+	int			Magic;	// 幻数。用于唯一标识
+	ByteArray	Data;	// 数据
+
+	// 初始化
+	WeakStore(int magic = 0, byte* ptr = NULL, uint len = 0);
+
+	// 检查并确保初始化，返回原来是否已初始化
+	bool Check();
+	void Init();
+
+    // 重载索引运算符[]，让它可以像数组一样使用下标索引。
+    byte& operator[](int i) const;
+};
+
+#endif
