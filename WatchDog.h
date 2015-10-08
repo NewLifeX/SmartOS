3e200eae93134f4aee5fdfa402e25d845a26c35c
nnhy <nnhy@vip.qq.com>
2015-10-08 12:58:17 +0000

    加大看门狗喂狗间隔

--- WatchDog.h
+++ WatchDog.h
@@ -14,12 +14,12 @@ public:
 
 	uint Timeout; // 当前超时时间
 
-	bool Config(uint ms);	// 配置看门狗喂狗重置时间，超过该时间讲重启MCU
+	bool Config(uint ms);	// 配置看门狗喂狗重置时间，超过该时间将重启MCU
 	void ConfigMax();		// 看门狗无法关闭，只能设置一个最大值
 	void Feed(); // 喂狗
 
 	// 打开看门狗。最长喂狗时间26208ms，默认2000ms
-	static void Start(uint msTimeout = 2000, uint msFeed = 100);
+	static void Start(uint msTimeout = 2000, uint msFeed = 500);
 };
 
 #endif
