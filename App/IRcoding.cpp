5c2eb748c0812455af26cd6ccffa7d906dfe3e30
WangQiang <807319473@qq.com>
2015-10-14 02:35:07 +0000

    添加红外编码储存文件  尚未写代码

---
+++ App/IRcoding.cpp
@@ -0,0 +1,15 @@
+﻿
+#include "IRcoding.h"
+
+
+IRcoding::IRcoding(FlashFace * flash)
+{
+	assert_param2(flash, "必须给出存储介质");
+	_Medium = flash;
+}
+
+IRcoding::~IRcoding()
+{
+}
+
+
---
+++ App/IRcoding.h
@@ -0,0 +1,24 @@
+﻿
+#ifndef __IRCODING_H__
+#define __IRCODING_H__
+
+#include "Sys.h"
+#include "..\Drivers\FlashFace.h"
+
+/*	
+	红外编码的处理，
+	读写Flash内的编码，与应用交互
+*/
+
+
+class IRcoding
+{
+public:
+	IRcoding(FlashFace * flash = NULL);
+	~IRcoding();
+	
+private:
+	FlashFace * _Medium;
+};
+
+#endif
