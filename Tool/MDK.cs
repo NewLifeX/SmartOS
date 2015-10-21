afcde3ca6b300f429197cc13b1ff27be927a6a57
nnhy <nnhy@vip.qq.com>
2015-10-21 02:53:11 +0000

    汇编也要支持Debug和Tiny

--- Tool/MDK.cs
+++ Tool/MDK.cs
@@ -235,6 +235,8 @@ namespace NewLife.Reflection
             {
                 sb.AppendFormat(" --pd \"{0} SETA 1\"", item);
             }
+            if(Debug) sb.Append(" --pd \"DEBUG SETA 1\"");
+			if(Tiny) sb.Append(" --pd \"TINY SETA 1\"");
 
             sb.AppendFormat(" --list \"{0}.lst\" --xref -o \"{1}.o\" --depend \"{1}.d\"", lstName, objName);
             sb.AppendFormat(" \"{0}\"", file);
