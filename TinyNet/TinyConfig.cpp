ae19ab5722dd8f04f23fa0e453ee399878f6f83f
nnhy <nnhy@vip.qq.com>
2015-10-22 08:24:05 +0000

    增加微网配置信息类

---
+++ TinyNet/TinyConfig.cpp
@@ -0,0 +1,19 @@
+﻿#include "TinyConfig.h"
+
+// 初始化
+TinyConfig::TinyConfig()
+{
+	// 实际内存大小，减去头部大小
+	uint len = sizeof(this) - ((int)&Length - (int)this);
+	//memset(&Length, 0, len);
+	Length = len;
+}
+
+void TinyConfig::LoadDefault()
+{
+	Kind	= Sys.Code;
+	//Server	= 0x01;
+
+	PingTime	= 15;
+	OfflineTime	= 60;
+}
---
+++ TinyNet/TinyConfig.h
@@ -0,0 +1,40 @@
+﻿#ifndef __TinyConfig_H__
+#define __TinyConfig_H__
+
+#include "Sys.h"
+#include "Stream.h"
+#include "Storage\Storage.h"
+
+// 必须设定为1字节对齐，否则offsetof会得到错误的位置
+#pragma pack(push)	// 保存对齐状态
+// 强制结构体紧凑分配空间
+#pragma pack(1)
+
+// 配置信息
+class TinyConfig
+{
+public:
+	ushort		Length;		// 数据长度
+
+	byte		HardVer;	// 硬件版本
+	byte		SoftVer;	// 软件版本
+	ushort		Kind;		// 类型
+	byte		Address;	// 分配得到的设备地址
+	byte		Password[16];	// 通信密码
+	byte		Server;		// 网关ID
+	byte		Channel;	// 通道
+	ushort		Speed;		// 传输速度
+	byte		ServerKey[16];	// 服务端组网密码，退网时使用。一般6字节
+
+	ushort		PingTime;	// 心跳时间。秒
+	ushort		OfflineTime;	// 离线阀值时间。秒
+	ushort		SleepTime;	// 睡眠时间。秒
+
+	// 初始化，各字段为0
+	TinyConfig();
+	void LoadDefault();
+};
+
+#pragma pack(pop)	// 恢复对齐状态
+
+#endif
