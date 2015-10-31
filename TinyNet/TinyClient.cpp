e31e6709b71d882b4356dee41293ada82415693e
cdyong <273797002@qq.com>
2015-10-31 07:29:34 +0000

    客户端非中继非组网网关不就受其它控制，测试通过

--- TinyNet/TinyClient.cpp
+++ TinyNet/TinyClient.cpp
@@ -110,9 +110,10 @@ bool TinyClient::OnReceive(TinyMessage& msg)
 	
 	// 不处理来自网关以外的消息
 	//if(Server == 0 || Server != msg.Dest) return true;
-	debug_printf("源地址: 0x%08X 网关地址:0x%08X \r\n",Server, msg.Src);
+	//debug_printf("源地址: 0x%08X 网关地址:0x%08X \r\n",Server, msg.Src);
 	
-	if(msg.Code != 0x01 && Server != 0 && Server != msg.Src) return true;//不是无线中继，不是组网消息。不是被组网网关消息，不受其它消息设备控制.
+	if(Type != 0x01C8)	
+	   if(msg.Code != 0x01&& Server != msg.Src) return true;//不是无线中继，不是组网消息。不是被组网网关消息，不受其它消息设备控制.
 	
 	if(msg.Src == Server) LastActive = Time.Current();
 
