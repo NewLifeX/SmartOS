c090ce2fbe9f235b7757876dbceedc7cea70332c
nnhy <nnhy@vip.qq.com>
2015-11-26 05:05:12 +0000

    很少用到TINY，暂时不参与编译

--- Tool/Build_SmartOS_F1x0.cs
+++ Tool/Build_SmartOS_F1x0.cs
@@ -40,13 +40,9 @@ namespace NewLife.Reflection
             build.CompileAll();
             build.BuildLib("..\\SmartOS_F1x0");
 
-			build.Tiny = true;
+			/*build.Tiny = true;
             build.CompileAll();
-            build.BuildLib("..\\SmartOS_F1x0");
-
-			//build.Defines.Add("TINY");
-            //build.CompileAll();
-            //build.BuildLib("..\\SmartOS_F0T");
+            build.BuildLib("..\\SmartOS_F1x0");*/
         }
     }
 
