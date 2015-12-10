44453917e5d6b9e193f028647849ecdbc726861e
cdyong <273797002@qq.com>
2015-12-10 06:23:28 +0000

    修改时间后，要马上跟新在线时间

--- TinyNet/TinyClient.cpp
+++ TinyNet/TinyClient.cpp
@@ -595,7 +595,7 @@ bool TinyClient::OnPing(const TinyMessage& msg)
 
 	if(msg.Src != Server) return true;
 
-	LastActive = Sys.Ms();
+	//LastActive = Sys.Ms();
 
 	// 处理消息
 	auto ms	= msg.ToStream();
@@ -612,6 +612,7 @@ bool TinyClient::OnPing(const TinyMessage& msg)
 				if(pm.ReadTime(ms, seconds))
 				{
 					Time.SetTime(seconds);
+					LastActive = Sys.Ms();
 				}
 				break;
 			}
