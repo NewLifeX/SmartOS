7c96abb1fcb2c9b62a3ffab90c646368993d542c
cdyong <273797002@qq.com>
2015-12-11 12:35:26 +0000

    相异和

---
+++ Security/CheckSum.cpp
@@ -0,0 +1,13 @@
+﻿#include "CheckSum.h"
+
+byte CheckSum::Sum(const Array& data)
+{
+	byte sum;
+	
+	for (int k = 0; k < data.Length(); k++)
+	{
+		sum^=data[k];
+	}
+	
+	return sum;
+}
---
+++ Security/CheckSum.h
@@ -0,0 +1,14 @@
+﻿#ifndef __CheckSum_H__
+#define __CheckSum_H__
+
+#include "Sys.h"
+
+//  相异和
+class CheckSum
+{
+public:
+	// 加解密
+	static byte Sum(const Array& data);
+};
+
+#endif
