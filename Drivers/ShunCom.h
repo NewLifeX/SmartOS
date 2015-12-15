25258873bb3eb8f5bb89a282c40e89720b20ee07
cdyong <273797002@qq.com>
2015-12-15 06:33:44 +0000

    还原

--- Drivers/ShunCom.h
+++ Drivers/ShunCom.h
@@ -8,11 +8,9 @@
 #include "Message\DataStore.h"
 
 // 上海顺舟Zigbee协议
-// 主站发送所有从站收到，从站发送只有主站收到
+// 主从模式时，主站可指定从站地址或广播，从站发送只有主站收到
 class ShunCom : public PackPort, public Power
 {
-private:
-
 public:
 	OutputPort	Reset;	// 复位
 	IDataPort*	Led;	// 指示灯
@@ -21,70 +19,40 @@ public:
 	OutputPort	Sleep;	// 睡眠
 	OutputPort	Config;	// 配置
 
-	InputPort	Run;	// 组网成功后闪烁
-	InputPort	Net;	// 组网中闪烁
-	InputPort	Alarm;	// 警告错误
+	byte	AddrLength;	// 地址长度。默认0，主站建议设为2
 
 	ShunCom();
 
 	void Init(ITransport* port, Pin rst = P0);
 
+	virtual string ToString() { return "ShunCom"; }
+
+	// 进入配置模式
+	bool EnterConfig();
+	// 退出配置模式
+	void ExitConfig();
+	// 读取配置信息
 	void ShowConfig();
+
+	// 设置设备的类型：00代表中心、01代表路由，02代表终端
+	void SetDevice(byte kind);
+	// 设置无线频点，注意大小端，ShunCom是小端存储
+	void SetChannel(byte chn);
+	// 设置发送模式00为广播、01为主从模式、02为点对点模式
+	void SetSend(byte mode);
+	// 进入配置PanID,同一网络PanID必须相同
+    void SetPanID(ushort id);
+
+private:
+	virtual bool OnOpen();
+    virtual void OnClose();
 
 	// 电源等级变更（如进入低功耗模式）时调用
 	virtual void ChangePower(int level);
-	//设置设备诶类型：00代表中心、01代表路由，02代表终端
-	virtual void SetDeviceMode(byte kind);
-	//设置无线频点，注意大小端，Zibeer是小端存储
-	virtual void SetChannel(Array& Channel);
-	//设置发送模式00为广播、01为主从模式、02为点对点模式
-	virtual void SetSendMode(byte mode);	
-	//进入配置PanID,同一网络PanID必须相同
-    virtual void SetPanID(Array& ID);
-	//进入配置模式
-	virtual bool EnterSetMode();
-	//退出配置模式
-	virtual void OutSetMode();
-	
-	//读取配置信息
-	virtual void ConfigMessage(ByteArray& buf);
-	
-    virtual bool OnWrite(const Array& bs);
+
 	// 引发数据到达事件
 	virtual uint OnReceive(Array& bs, void* param);
 
-	virtual string ToString() { return "ShunCom"; }
-	virtual bool OnOpen();
-
-protected:
-	
-    virtual void OnClose();
-};
-
-class ShunComMessage
-{	
-public:
-	byte 		Frame;		      //帧头
-	short		Length;		      //数据长度
-	short		Code;		      //操作码
-	short		CodeKind;  	      //操类型
-	short		DataLength;	      //负载数据长度
-	byte   		Data[64];	      // 负载数据部分
-	byte		Checksum;	      //异或校验、从数据长度到负载数据尾
-	Array		*Buf;
-	
-public:    
-                       
-	ShunComMessage();
-	ShunComMessage(short code,short codeKind);
-	
-	~ShunComMessage();
-	virtual bool Read(Stream& ms);
-	// 写入指定数据流
-	virtual void Write(Stream& ms) const;
-	// 验证消息校验码是否有效
-	virtual bool Valid();
-	// 显示消息内容
-	virtual void Show() const;
+	virtual bool OnWriteEx(const Array& bs, void* opt);
 };
 #endif
