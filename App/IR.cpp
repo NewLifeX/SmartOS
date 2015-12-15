a17a31bdd449b266d5fac9e066d7b9efed94fa67
WangQiang <807319473@qq.com>
2015-12-15 11:46:25 +0000

    不能使用 定时器中断作为数据接收结束条件   每次拿到数据清零计数器的同时会触发中断 现在拿到的数据 间隔一个字节就会出现零  怀疑DMA 传送数据以HalfWord对齐

--- App/IR.cpp
+++ App/IR.cpp
@@ -45,14 +45,16 @@ IRStat Stat;
 int IR::Receive(Array& bs, int sTimeout)
 {
 	TS("IR::Receive");
-	// 捕获设置
+	
+	uint bufLen	= bs.Length();
 	
 	if(_Tim == NULL)
 	{
+		RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
 		_Tim = Timer::Create(0x01);		// 直接占用TIMER2
-		// _Tim->SetFrequency(50);			// 5Hz   20ms
+		// _Tim->SetFrequency(50);		// 5Hz   20ms
 		// 使用一个字节  需要自行配置分频系数
-		_Tim->Prescaler	= 4392;		// 分频 需要结果是 Period < 256 时能计数 20ms
+		_Tim->Prescaler	= 800;		// 分频 需要结果是 Period < 256 时能计数 20ms
 		_Tim->Period	= 255;
 		_Tim->Config();
 	}
@@ -65,8 +67,10 @@ int IR::Receive(Array& bs, int sTimeout)
 		_Port->AFConfig(GPIO_AF_2);
 	}
 	
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
-	
+	/* 使能自动装载 */
+	TIM_ARRPreloadConfig(TIM2, ENABLE);
+	// 捕获设置
+	// 2通道映射到1通道   配套使用DMA1 Channel5
 	TIM_ICInitTypeDef  TIM_ICInitStructure;  
 	TIM_ICInitStructure.TIM_Channel		= TIM_Channel_1;
 	TIM_ICInitStructure.TIM_ICPolarity	= TIM_ICPolarity_BothEdge;
@@ -76,35 +80,26 @@ int IR::Receive(Array& bs, int sTimeout)
 	TIM_PWMIConfig(TIM2, &TIM_ICInitStructure);
 	
 	/* Select the TIM2 Input Trigger: TI2FP2 */
-	TIM_SelectInputTrigger(TIM2, TIM_TS_ITR0);
-	/* Select the slave Mode: Reset Mode */
+	TIM_SelectInputTrigger(TIM2, TIM_TS_TI2FP2);
+	/* 捕获的同时清空定时器计数 */
 	TIM_SelectSlaveMode(TIM2, TIM_SlaveMode_Reset);
 	//TIM_SelectMasterSlaveMode(TIM2,TIM_MasterSlaveMode_Enable);
 	
-	/* 使能自动装载 */
-	TIM_ARRPreloadConfig(TIM2, ENABLE);
-	/* TIM enable counter */
 	//TIM_Cmd(TIM2, ENABLE);
 	
-	/* DMA1 clock enable */
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1 , ENABLE);
-	
+	// 连接DMA 触发机制
 	TIM_DMACmd(TIM2, TIM_DMA_CC1, ENABLE);
 	
-	//bs.SetLength(512);
-	byte buf[512];
-	
-	// GD32  DMA Channel3 有 TIM2_CH2
-	// STM32 DMA 没有 TIM2_CH2 触发源
-
+	// 使用通道5
 	/* DMA1 Channel1 Config */
 	DMA_InitTypeDef     DMA_InitStructure;
 	DMA_DeInit(DMA1_Channel5);
 	DMA_InitStructure.DMA_PeripheralBaseAddr	= (uint32_t)&(TIM2->CNT);
-	//DMA_InitStructure.DMA_MemoryBaseAddr 		= (uint32_t)bs.GetBuffer();
-	DMA_InitStructure.DMA_MemoryBaseAddr 		= (uint32_t)buf;
+	DMA_InitStructure.DMA_MemoryBaseAddr 		= (uint32_t)bs.GetBuffer();
+	//DMA_InitStructure.DMA_MemoryBaseAddr 		= (uint32_t)buf;
 	DMA_InitStructure.DMA_DIR 					= DMA_DIR_PeripheralSRC;
-	DMA_InitStructure.DMA_BufferSize 			= 512;
+	DMA_InitStructure.DMA_BufferSize 			= bufLen;
 	DMA_InitStructure.DMA_PeripheralInc 		= DMA_PeripheralInc_Disable;
 	DMA_InitStructure.DMA_MemoryInc 			= DMA_MemoryInc_Enable;
 	DMA_InitStructure.DMA_PeripheralDataSize 	= DMA_PeripheralDataSize_Byte;
@@ -114,63 +109,57 @@ int IR::Receive(Array& bs, int sTimeout)
 	DMA_InitStructure.DMA_M2M 					= DMA_M2M_Disable;
 	DMA_Init(DMA1_Channel5, &DMA_InitStructure);
   
-  
-	/* DMA1 Channel1 enable */
+	// 开启DMA TIME
 	DMA_Cmd(DMA1_Channel5, ENABLE);
-	///* TIM enable counter */
 	TIM_Cmd(TIM2, ENABLE);
 	
 	Stat = WaitRev;
 	// 等待起始条件
-	TimeWheel tw(10);
-	while(!tw.Expired());
-	// && DMA_GetCurrDataCounter(DMA1_Channel5) == bs.Length()
-	if(tw.Expired()) 
+	TimeWheel tw(sTimeout);
+	while(!tw.Expired() && DMA_GetCurrDataCounter(DMA1_Channel5) == bufLen);
+
+	if(tw.Expired()) 	// 超时没有收到学习
 	{	
 		Stat = RevOver;
-		
-		/* DMA1 Channel1 enable */
+		// 关闭Time DMA
 		DMA_Cmd(DMA1_Channel5, DISABLE);
-		/* TIM enable counter */
 		TIM_Cmd(TIM2, DISABLE);
-		
 		
 		int leng = DMA_GetCurrDataCounter(DMA1_Channel5);
 		debug_printf("\r\n DMA::Counter %d \r\n", leng);
 		
-		//bs.SetLength(0);
-		
+		bs.SetLength(0);
 		return -1;
 	}
+	// 进入接收状态
 	Stat = Rev;
-	// 启动终止条件触发
-	_Tim->Register(OnReceive,this);
-	
-	
-	tw.Reset(0,200);	// 300ms
+
+	debug_printf("\r\n开始接收数据\r\n");
+	// 300ms 接收时间
+	tw.Reset(1);
 	while(!tw.Expired() && Stat != RevOver);
 	
-	_Tim->Register(NULL,NULL);
-	
-	/* DMA1 Channel1 enable */
+	// 关闭Time DMA	
 	DMA_Cmd(DMA1_Channel5, DISABLE);
-	/* TIM enable counter */
 	TIM_Cmd(TIM2, DISABLE);
 	
-	if(Stat != RevOver)
+	int len = bufLen - DMA_GetCurrDataCounter(DMA1_Channel5);
+	if(len > 30)
 	{
+		debug_printf("收到数据 %d byte\r\n",len);
 		Stat = RevOver;
-		//bs.SetLength(0);
+		bs.SetLength(len);
+		return len;
+	}
+	else
+	{
+		debug_printf("未收到数据 %d byte\r\n",len);
+		Stat = RevOver;
+		bs.SetLength(0);
 		return -1;
 	}
-	
-	int len = DMA_GetCurrDataCounter(DMA1_Channel5);
-	//bs.SetLength(len);
-	
-	return len;
 }
 
 void IR::OnReceive(void* sender, void* param)
 {
-	Stat = RevOver;
 }
