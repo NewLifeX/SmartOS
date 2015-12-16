26f983cb51d2a219b4140fa931f270b361b3da14
nnhy <nnhy@vip.qq.com>
2015-12-16 03:07:14 +0000

    新增DHT11驱动，编译通过，未测试

---
+++ Drivers/DHT11.cpp
@@ -0,0 +1,94 @@
+﻿#include "DHT11.h"
+
+DHT11::DHT11(Pin da)
+{
+	DA.Set(da);
+}
+
+DHT11::~DHT11()
+{
+	DA.Close();
+}
+
+void DHT11::Init()
+{
+	debug_printf("\r\nDHT11::Init DA=%s \r\n", DA.ToString().GetBuffer());
+
+	DA.Open();
+}
+
+bool DHT11::Read(ushort& temp, ushort& humi)
+{
+	if(DA.Empty()) return false;
+
+	/*输出模式*/
+	/*主机拉低*/
+	DA	= false;
+	/*延时18ms*/
+	Sys.Sleep(18);
+
+	/*总线拉高 主机延时30us*/
+	DA	= true;
+	Sys.Delay(30);   //延时30us
+
+	/*主机设为输入 判断从机响应信号*/
+
+	/*判断从机是否有低电平响应信号 如不响应则跳出，响应则向下运行*/
+	if(DA.ReadInput() != false) return false;
+
+	/*轮询直到从机发出 的80us 低电平 响应信号结束*/
+	while(!DA.ReadInput());
+
+	/*轮询直到从机发出的 80us 高电平 标置信号结束*/
+	while(DA.ReadInput());
+
+	/*开始接收数据*/
+	humi	= ReadByte() << 8;
+	humi	|= ReadByte();
+
+	temp	= ReadByte() << 8;
+	temp	|= ReadByte();
+
+	byte sum= ReadByte();
+
+	/*读取结束，引脚改为输出模式*/
+	/*主机拉高*/
+	DA	= true;
+
+	/*检查读取的数据是否正确*/
+	return sum == (humi >> 8) + (humi && 0xFF) + (temp >> 8) + (temp && 0xFF);
+}
+
+byte DHT11::ReadByte()
+{
+	byte temp = 0;
+	for(int i=0; i<8; i++)
+	{
+		/*每bit以50us低电平标置开始，轮询直到从机发出 的50us 低电平 结束*/
+		while(!DA.ReadInput());
+
+		/*DHT11 以27~28us的高电平表示“0”，以70us高电平表示“1”，
+		通过检测60us后的电平即可区别这两个状态*/
+
+		Sys.Delay(60); //延时60us
+
+		if(DA.ReadInput())//60us后仍为高电平表示数据“1”
+		{
+			/*轮询直到从机发出的剩余的 30us 高电平结束*/
+			while(DA.ReadInput());
+
+			temp	|= (byte)(0x01 << (7-i));  // 把第7-i位置1
+		}
+		else	 //60us后为低电平表示数据“0”
+		{
+			temp	&= (byte)~(0x01 << (7-i)); // 把第7-i位置0
+		}
+	}
+	return temp;
+}
+
+void DHT11::ChangePower(int level)
+{
+	// 进入低功耗时，直接关闭
+	if(level) DA.Close();
+}
---
+++ Drivers/DHT11.h
@@ -0,0 +1,26 @@
+﻿#ifndef _DHT11_H_
+#define _DHT11_H_
+
+#include "I2C.h"
+#include "Power.h"
+
+// 温湿度传感器
+class DHT11
+{
+public:
+	OutputPort DA;	// 数据总线
+
+    DHT11(Pin da);
+    ~DHT11();
+
+	void Init();
+	bool Read(ushort& temp, ushort& humi);
+
+	// 电源等级变更（如进入低功耗模式）时调用
+	virtual void ChangePower(int level);
+
+private:
+	byte ReadByte();
+};
+
+#endif
