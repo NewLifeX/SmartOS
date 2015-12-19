cbd2a0c827ebdc7a8d1f8743e89035c1b21946a7
WangQiang <807319473@qq.com>
2015-12-19 10:34:33 +0000

    74HC165驱动程序

---
+++ Drivers/74HC165.cpp
@@ -0,0 +1,50 @@
+﻿#include "74HC165.h"
+
+
+IC74HC165::IC74HC165(Pin pl, Pin sck, Pin in, Pin ce) 
+					: _PL(pl),_SCK(sck),_In(in)
+{
+	_PL.Invert = true;
+	
+	_PL.Open();
+	_SCK.Open();
+	_In.Open();
+	
+	if(ce != P0)
+	{
+		_CE = new OutputPort(ce);
+		_CE.Invert = true;
+		_CE.Open();
+	}
+}
+
+byte IC74HC165::Read(byte *buf, byte count)
+{
+	if(!buf)return;
+	_PL = false;	// 不采集
+	if(_CE) _CE	= true;	// 使能芯片
+	_PL = true;		// 采集
+	_PL = false;	// 采集
+	
+	_SCK = false;
+	
+	for(int i = 0; i < count; i++)
+	{
+		byte temp;
+		for(int j = 0; j < 8; j++)
+		{
+			temp <<= 1;
+			_SCK = true;
+			if(_In) temp |= 0x01;
+			else	temp &= 0xfe;
+			_SCK = false;
+		}
+		
+		*buf = temp;
+		buf++;
+	}
+	if(_CE) _CE = false;
+}
+
+
+
---
+++ Drivers/74HC165.h
@@ -0,0 +1,25 @@
+﻿#ifndef _74HC165_H_
+#define _74HC165_H_
+
+#include "Port.h"
+#include "Power.h"
+
+// 温湿度传感器
+class IC74HC165
+{
+private:
+	OutputPort	_PL;
+	OutputPort	_SCK;
+	InputPort	_In;
+	OutputPort * _CE = NULL;
+	
+public:
+
+	// pl采集，cp时钟，Q7输出，ce使能
+	IC74HC165(Pin pl, Pin sck, Pin in, Pin ce = P0);
+	// 级联读取  buf,数据放置位置，count，级联ic数
+	byte Read(byte *buf = NULL, byte count);
+	
+};
+
+#endif
