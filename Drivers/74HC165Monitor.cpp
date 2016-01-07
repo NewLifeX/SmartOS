0f0f87cba178dfc2bbec3c59b1e7f5242a441992
WangQiang <807319473@qq.com>
2016-01-07 02:45:01 +0000

    被动读取 74HC165 数据  监听

---
+++ Drivers/74HC165Monitor.cpp
@@ -0,0 +1,100 @@
+﻿#include "74HC165Monitor.h"
+
+
+IC74HC165MOR::IC74HC165MOR(Pin pl, Pin sck, Pin in, byte bufSize,bool highSpeed) 
+					: _PL(pl),_SCK(sck),_In(in),_Bs(bufSize)
+{
+	_PL.Invert = true;
+	_sckHighSpeed = highSpeed;
+	Opened = false;
+}
+
+bool IC74HC165MOR::Open()
+{
+	if(Opened)return true;
+	
+	_PL.HardEvent = true;	// 硬中断
+	_PL.Mode = InputPort::Rising;		// 上升沿
+	_PL.Register([](InputPort* port, bool down, void* param){ ((IC74HC165MOR*)param)->Trigger();},this);
+	_PL.Open();
+	
+	if(!_sckHighSpeed)
+	{
+		_PL.HardEvent = true;	// 硬中断
+		_SCK.Mode = InputPort::Rising;		// 上升沿
+		_SCK.Register([](InputPort* port, bool down, void* param){ ((IC74HC165MOR*)param)->ReaBit();},this);
+	}
+	_SCK.Open();
+	_In.Open();
+	Opened = true;
+	return true;
+}
+
+bool IC74HC165MOR::Close()
+{
+	_PL.Close();
+	_SCK.Close();
+	_In.Close();	
+	Opened = false;
+	return true;
+}
+
+void IC74HC165MOR::Trigger()
+{
+	if(!_sckHighSpeed)
+	{
+		_irqCount = 0;
+		return;
+	}
+	int bits = _Bs.Length() * 8;	// 总位数
+	for(int i = 0; i < _Bs.Length(); i++)
+	{
+		byte temp = 0x00;
+		TimeWheel tw(0,2);
+		for(int j = 0; j < 8; j++)
+		{
+			while(!tw.Expired() && _SCK != true );	// 等待 高电平
+			
+			if(tw.Expired()) break;
+		
+			if(_In) temp |= 0x01;
+			else	temp &= 0xfe;
+			
+			while(!tw.Expired() && _SCK != false );	// 等待低电平
+			
+			temp <<= 1;
+		}
+		_Bs[i] = temp;
+		if(tw.Expired())return;	 // 2ms 不结束 就强制退出
+	}
+}
+
+void IC74HC165MOR::ReaBit()
+{
+	byte temp = _Bs[_irqCount/8];
+	if(_In)
+	{
+		byte x = 0x01 << (_irqCount >> 3);
+		temp |= x;
+	}
+	else
+	{
+		byte x = ~(0x01 << (_irqCount >> 3));
+		temp &= x;
+	}
+	
+	_Bs[ _irqCount >> 3 ] = temp;
+	_irqCount++;
+}
+
+byte IC74HC165MOR::Read(byte *buf, byte count)
+{
+	Open();
+	return _Bs.CopyTo(buf,count);
+}
+
+byte IC74HC165MOR::Read()
+{
+	Open();
+	return *_Bs.GetBuffer();
+}
---
+++ Drivers/74HC165Monitor.h
@@ -0,0 +1,40 @@
+﻿#ifndef _74HC165MOR_H_
+#define _74HC165MOR_H_
+
+#include "Port.h"
+#include "Power.h"
+#include "Time.h"
+
+/* 备注
+调用构造函数后 需要Open()一下
+如果不Open()  在Read 里面也会Open  但第一次读取不对
+*/
+class IC74HC165MOR
+{
+private:
+	InputPort	_PL;
+	InputPort	_SCK;
+	InputPort	_In;
+	
+	ByteArray _Bs;		// 内部缓冲区
+	byte 	_irqCount;	// 记录pl信号后第几位数据
+	bool	_sckHighSpeed;
+public:
+	bool Opened;
+	// pl采集，cp时钟，Q7输出， bufsize 是级联个数，highSpeed 是否是高速clk ms以下算高速
+	IC74HC165MOR(Pin pl, Pin sck, Pin in, byte bufSize = 0,bool highSpeed = true);
+	
+	bool Open();
+	bool Close();
+	
+	// 内部中断委托函数
+	void Trigger();
+	void ReaBit();
+	
+	// 级联读取  buf,数据放置位置，count，级联ic数
+	byte Read(byte *buf, byte count);
+	// 读一个IC 的数据
+	byte Read();
+};
+
+#endif
