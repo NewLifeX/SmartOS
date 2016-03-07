9b26ca05466f627fb4cfe39cd4f1e1c496cbe09a
WangQiang <807319473@qq.com>
2016-03-07 08:48:08 +0000

    XH711   24位 ADC   电子称使用IC

---
+++ Drivers/XH711.h
@@ -0,0 +1,26 @@
+﻿#ifndef _XH711_H_
+#define _XH711_H_
+
+#include "Port.h"
+#include "Power.h"
+
+// 24位 ADC
+class XH711
+{
+private:
+
+	
+public:
+	bool Opened;
+
+	XH711();
+	
+	bool Open();
+	bool Close();
+	
+	bool Read(byte *buf);
+
+	uint Read();
+};
+
+#endif
