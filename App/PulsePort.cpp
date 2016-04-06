25e04b1b0d287a9448678d765bc205093bf39bf1
WangQiang <807319473@qq.com>
2016-04-06 12:13:56 +0000

    脉冲输入端口  带去抖功能

---
+++ App/PulsePort.cpp
@@ -0,0 +1,111 @@
+﻿
+#include "PulsePort.h"
+
+PulsePort::PulsePort(){}
+
+PulsePort::PulsePort(Pin pin)
+{
+	if(pin == P0)return;
+	_Port = new InputPort(pin);
+}
+
+bool PulsePort::Set(InputPort * port, uint intervals, uint shktime)
+{
+	if(port == nullptr)return false;
+	_Port = port;
+	Intervals = intervals;
+	ShakeTime = shktime;
+	return true;
+}
+
+void PulsePort::Open()
+{
+	if(_Port == nullptr)return;
+	if(Handler == nullptr)return;
+	
+	ShkPulse = ShakeTime/Intervals;
+	
+	if(Opened)return;
+	_Port->HardEvent = true;
+	_Port->Register(
+			[](InputPort* port, bool down, void* param){((PulsePort*)param)->OnHandler(port,down);},
+			this);
+	_Port->Open();
+	
+	_task = Sys.AddTask(
+				[](void* param)
+						{
+						auto port = (PulsePort*)param;
+						
+						// 从无到有一定是去抖的结果
+						// 从有到无一定是超时的结果
+						// 不是去抖的结果 肯定是从有到无
+						if(port->ShkStat== false)
+							port->value = false;
+						
+						Sys.SetTask(port->_task,false);
+						port->Handler(port,port->value,port->Param);
+						},
+				this, ShakeTime, ShakeTime, "PulsePort触发任务");
+}
+
+void PulsePort::Close()
+{
+	_Port->Close();
+	delete _Port;
+}
+
+void PulsePort::Register(PulsePortHandler handler, void* param)
+{
+	if(handler)
+	{
+		Handler = handler;
+		Param	= param;
+	}
+}
+
+void PulsePort::OnHandler(InputPort* port,bool down)
+{
+	if(down)return;
+	UInt64 now = Sys.Ms();
+	
+	if(value)
+	{
+		LastTriTime = now;
+		// 有连续脉冲情况下  一定是不用去抖的
+		ShkStat = false;
+		// 丢失脉冲，使用定时器来做  如果定时到了 说明中间都没有脉冲
+		if(_task)Sys.SetTask(_task, true, ShakeTime);
+	}
+	else
+	{
+		// 丢失脉冲后 重新来脉冲需要考虑 去抖 （也许是干扰脉冲）
+		if(!ShkStat)	// 断掉脉冲 && Shake去抖没开始
+		{
+			ShkTmeStar = now;
+			ShkStat = true;
+			ShkCnt = 1;
+		}
+		else
+		{
+			if(now >= ShkTmeStar + ShakeTime)
+			{		// ShakeTime 已过
+				if(ShkCnt < ShkPulse)
+				{	// 去抖结果为失败
+					ShkTmeStar = now;
+					ShkCnt = 1;
+					return;
+				}
+				LastTriTime = ShkTmeStar;
+				value = true;			// 有脉冲
+				// 触发
+				if(_task)Sys.SetTask(_task, true, -1);
+				return;
+			}
+			else	// 还在	ShakeTime 之中
+			{
+				ShkCnt++;
+			}
+		}
+	}
+}
---
+++ App/PulsePort.h
@@ -0,0 +1,48 @@
+﻿
+#ifndef __Pulse_Port_H__
+#define __Pulse_Port_H__
+
+#include "Sys.h"
+#include "Timer.h"
+#include "Port.h"
+
+
+// 输入类型为脉冲的IO驱动
+class PulsePort
+{
+    typedef void (*PulsePortHandler)(PulsePort* port, bool down, void* param);
+private:
+	bool		Opened 	= false;
+	bool		value	= false;	// 对外的状态
+	uint		ShkPulse= 0;		// ShakeTime/Intervals/2 即 去抖脉冲个数 提前计算降低中断代价
+	uint		ShkCnt 	= 0;		// ShakeTime 期间脉冲计数
+	
+	uint		_task 	= 0;		// 任务
+	
+	UInt64 	ShkTmeStar;				// 去抖起动时刻
+    PulsePortHandler	Handler	= nullptr;
+	void*				Param	= nullptr;
+	
+public:
+	PulsePort();					// 默认浮空输入
+	PulsePort(Pin pin);				// 默认浮空输入
+	bool Set(InputPort * port = nullptr, uint intervals = 1, uint shktime = 0);
+	
+	// 如果是us  就没有程序处理的必要了  速度太快 会严重占用CPU时间的！！！
+	// 所以是不接受 us脉冲间隔的脉冲IO输入设备
+	
+	uint Intervals	= 1;			// 脉冲间隔 单位 ms   即 1/Period  最低1ms 
+	uint ShakeTime	= 0;			// 去抖时间
+	
+	void Open();
+	void Close();
+	
+	void OnHandler(InputPort* port,bool down);		// 内部中断函数
+	void Register(PulsePortHandler handler = NULL, void* param = NULL);
+	
+	InputPort *	_Port 	= nullptr;
+	UInt64 		LastTriTime;		// 最后一次触发时间
+	bool 	ShkStat;				// 去抖标志
+};
+
+#endif 
