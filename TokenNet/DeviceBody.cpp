38848c1df64d9c9485ab8ed2bedd2a5b826dc004
WangQiang <807319473@qq.com>
2016-04-11 01:26:25 +0000

    网关内部虚拟设备实体  未完

---
+++ TokenNet/DeviceBody.cpp
@@ -0,0 +1,177 @@
+﻿
+#include "Config.h"
+
+#include "TokenNet\Token.h"
+#include "Security\RC4.h"
+
+#include "DeviceBody.h"
+#include "TinyNet\DataMessage.h"
+
+DeviceBody::DeviceBody(BodyManagement* mgmt)
+{
+	if (mgmt)Mgmt = mgmt;
+
+	// 直接使用 Device 类的数据区
+	Store.Data.Set(DevInfo._Store, sizeof(DevInfo._Store));
+}
+
+bool DeviceBody::SetSecDaStor(uint addr, byte * buf, int len)
+{
+	if (addr < Store.Data.Length || addr > 0x8000000)
+	{
+		debug_printf("DeviceBody::SetSecDaStor 地址设置不合法\r\n");
+		return false;
+	}
+	if (buf == nullptr || len == 0 || len > 2048)return false;
+	Store2 = new DataStore();
+	Store2->Data.Set(buf, len);
+	Store2->VirAddrBase = addr;
+	return true;
+}
+
+bool DeviceBody::SetSecDaStor(DataStore * store)
+{
+	if (store->VirAddrBase + store->Data.Length() > 0x8000000 || store->VirAddrBase < Store.Data.Length)
+	{
+		debug_printf(" DeviceBody::SetSecDaStor 虚拟地址不正确\r\n");
+	}
+	Store2 = store;
+	return true;
+}
+
+// byte * DeviceBody::GetData()
+// {
+// 	if (DevInfo._Store == Store.Data.GetBuffer)
+// 		return Store.Data.GetBuffer();
+// 	return nullptr;
+// }
+
+void DeviceBody::Open()
+{
+	if (Opened)return;
+	debug_printf("DeviceBody::Open()  ");
+
+	if (Mgmt==nullptr)
+	{
+		debug_printf("BodyMgmt 错误\r\n");
+		return;
+	}
+
+	if (OnOpen)
+	{
+		// 调用子类 Open 函数
+		if (!OnOpen())return;
+	}
+
+	if (DevInfo.Kind == 0)
+	{
+		debug_printf("请明确设备类型\r\n");
+		return;
+	}
+
+	// auto pMgmt = DevicesManagement::Current;
+	// if (!pMgmt)
+	// {
+	// 	debug_printf("没有 DevicesManagement\r\n");
+	// 	return;
+	// }
+	// // 把自己装入设备列表里面  并且一直在线
+	// pMgmt->PushDev(&DevInfo);
+	// pMgmt->OnlineAlways.Push(&DevInfo);
+
+	debug_printf("\r\n");
+	Opened = true;
+}
+
+void DeviceBody::Close()
+{
+	if (!Opened)return;
+	debug_printf("DeviceBody::Close()  ");
+
+	if (OnClose)
+	{
+		// 调用子类 Open 函数
+		if (!OnClose())return;
+	}
+	// // 把自己装入设备列表里面  并且一直在线
+	// auto pMgmt = DevicesManagement::Current;
+	// pMgmt->DeleteDev(DevInfo.Kind);
+
+	debug_printf("\r\n");
+	Opened = false;
+}
+
+void DeviceBody::OnWrite(const TokenMessage & msg)
+{
+	if (msg.Reply) return;
+	if (msg.Length < 2) return;
+	TS("DeviceBody::OnWrite");
+
+	TokenMessage rs;
+	rs.Code = msg.Code;
+	rs.Reply = true;
+	rs.State = msg.State;
+	//rs.Seq = msg.Seq;
+
+	auto ms = rs.ToStream();
+	DataMessage dm(msg, &ms, true);
+
+	bool rt = true;
+	if (dm.Offset < 64)
+		rt = dm.WriteData(Store, false);
+	else if (dm.Offset < 128)
+	{
+		dm.Offset -= 64;
+		//rt	= dm.WriteData(dv.GetConfig(), false);
+	}
+	else if(Store2 && dm.Offset > Store2->VirAddrBase)
+	{
+		rt = dm.WriteData(*Store2, false);
+	}
+
+	rs.Error = !rt;
+	rs.Length = ms.Position();
+
+	// Reply(rs);
+}
+
+void DeviceBody::OnRead(const TokenMessage & msg)
+{
+	if (msg.Reply) return;
+	if (msg.Length < 2) return;
+
+	TokenMessage rs;
+	rs.Code = msg.Code;
+	rs.Reply = true;
+	rs.State = msg.State;
+	//rs.Seq = msg.Seq;
+
+	auto ms = rs.ToStream();
+
+	DataMessage dm(msg, &ms, true);
+
+	bool rt = true;
+	if (dm.Offset < 64)
+		rt = dm.ReadData(Store);
+	else if (dm.Offset < 128)
+	{
+		//dm.Offset -= 64;
+		//Buffer bs(Cfg, Cfg->Length);
+		//rt = dm.ReadData(bs);
+	}
+	else if (Store2 && dm.Offset > Store2->VirAddrBase)
+	{
+		rt = dm.ReadData(*Store2);
+	}
+
+	rs.Error = !rt;
+	rs.Length = ms.Position();
+
+	// Reply(rs);
+}
+
+
+
+
+
+
---
+++ TokenNet/DeviceBody.h
@@ -0,0 +1,55 @@
+﻿#ifndef __DeviceBody_H__
+#define __DeviceBody_H__
+
+#include "Sys.h"
+#include "Message\DataStore.h"
+
+//#include "TokenNet\TokenClient.h"
+
+#include "TinyNet\Device.h"
+#include "TinyNet\DevicesManagement.h"
+
+// tokendistributive
+
+
+// 令牌网
+class DeviceBody
+{
+private:
+	bool Opened = false;
+	// 第二数据区起始地址
+	uint St2BaseAddr;
+public:
+	Device DevInfo;
+	DataStore Store;
+
+	// 第二数据区 
+	DataStore * Store2;
+
+	DeviceBody();
+	// 设置第二数据区
+	bool SetSecDaStor(uint addr,byte* buf,int len);
+	bool SetSecDaStor(DataStore * store);
+
+	// 获取数据区指针
+	// byte* GetData();
+
+	void Open();
+	virtual bool OnOpen() = 0;
+	void Close();
+	virtual bool OnClose() = 0;
+
+	// 消息收发器 对其进行读写
+	void OnWrite(const TokenMessage& msg);
+	void OnRead(const TokenMessage& msg);
+
+	// 直接找分发器的发送函数就好 
+	// bool Send(TokenMassge &msg);
+	// bool Reply(TokenMassge &msg);
+
+	// 主动上报数据区数据    注意，这里 offset 为虚拟地址  特别小心 DOF（xx） 
+	bool Report(uint offset, byte dat);
+	bool Report(uint offset, const Buffer& bs);
+};
+
+#endif
