abce270a355a7e436fa67df9d72fa1ba3f359c5a
WangQiang <807319473@qq.com>
2016-05-21 15:47:12 +0000

    基本指令  获取/设置 模式   设置回显  Join Wifi  UnJoin Wifi 测试通过

--- Drivers/Esp8266.cpp
+++ Drivers/Esp8266.cpp
@@ -1,291 +1,429 @@
-#include "Message\DataStore.h"
-#include "Esp8266.h"
+﻿
+#include "ESP8266.h"
 
-#define NET_DEBUG DEBUG
-//#define NET_DEBUG 0
-#if NET_DEBUG
-	#define net_printf debug_printf
-#else
-	#define net_printf(format, ...)
-#endif
-
-/******************************** 内部Tcp/Udp ********************************/
-class EspSocket : public Object, public ITransport, public ISocket
+void EspTest(void * param)
 {
-private:
-	Esp8266&	_Host;
+	AiESP esp(COM4);
+	esp.Port.SetBaudRate(115200);
+	esp.Port.Open();
 
-public:
-	EspSocket(Esp8266& host, ProtocolType protocol);
-	virtual ~EspSocket();
+	Sys.Sleep(50);				//
 
-	// 应用配置，修改远程地址和端口
-	void Change(const IPEndPoint& remote);
+	if (esp.GetMode() != Station)	// Station模式
+		esp.SetMode(Station);
 
-	virtual bool OnWrite(const Buffer& bs);
-	virtual uint OnRead(Buffer& bs);
+	String ate = "ATE1";			// 开回显
+	esp.SendCmd(ate);
 
-	// 发送数据
-	virtual bool Send(const Buffer& bs);
-	// 接收数据
-	virtual uint Receive(Buffer& bs);
-	
-};
+	//String ssid = "yws007";
+	//String pwd = "yws52718";
 
-class EspTcp : public EspSocket
-{
-public:
-	EspTcp(Esp8266& host);
-};
-
-class EspUdp : public EspSocket
-{
-public:
-	EspUdp(Esp8266& host);
-
-	virtual bool SendTo(const Buffer& bs, const IPEndPoint& remote);
-
-	// 中断分发  维护状态
-	virtual void OnProcess(byte reg);
-	// 用户注册的中断事件处理 异步调用
-	virtual void RaiseReceive();
-
-	virtual String& ToStr(String& str) const { return str + "Udp_" + Local.Port; }
-
-private:
-	virtual bool OnWriteEx(const Buffer& bs, const void* opt);
-};
-
-/******************************** Esp8266 ********************************/
-
-Esp8266::Esp8266(ITransport* port, Pin rst)
-{
-	Set(port);
-    
-	if(rst != P0) _rst.Set(rst);
+	String ssid = "FAST_2.4G";
+	String pwd = "yws52718*";
+	bool isjoin = esp.JoinAP(ssid, pwd);
+	if (isjoin)debug_printf("Join ok\r\n");
+	esp.UnJoinAP();
+	Sys.Sleep(1000);
 }
 
-bool Esp8266::OnOpen()
+String busy = "busy p...";
+String discon = "WIFI DISCONNECT";
+String conn = "WIFI CONNECTED";
+String okstr = "OK";
+String gotIp = "WIFI GOT IP";
+
+
+AiESP::AiESP()
 {
-	if(!PackPort::OnOpen()) return false;
+	Mode = Unknown;
+}
 
-	if(!_rst.Empty())
+AiESP::AiESP(COM com, AiEspMode mode)
+{
+	Port.Set(com);
+	Mode = mode;
+}
+/*
+发送：
+"AT+CWMODE=1
+"
+返回：
+"AT+CWMODE=1
+
+OK
+"
+*/
+bool AiESP::SetMode(AiEspMode mode)
+{
+	String str = "AT+CWMODE=";
+	String cmd;
+	switch (mode)
 	{
-		_rst.Open();
+	case Unknown:
+		return false;
+		//break;
+	case Station:
+		cmd = str + '1';
+		break;
+	case Ap:
+		cmd = str + '2';
+		break;
+	case Both:
+		cmd = str + '3';
+		break;
+	default:
+		return false;
 
-		_rst = true;
-		Sys.Delay(100);
-		_rst = false;
-		Sys.Delay(100);
-		_rst = true;
 	}
-
-	return true;
-}
-
-void Esp8266::OnClose()
-{
-	_rst.Close();
-
-	PackPort::OnClose();
-}
-
-// 配置网络参数
-void Esp8266::Config()
-{
-	
-}
-
-ISocket* Esp8266::CreateSocket(ProtocolType type)
-{
-	switch(type)
+	if (SendCmd(cmd))
 	{
-		case ProtocolType::Tcp:
-			return new EspTcp(*this);
+		Mode = mode;
+		return true;
+	}
+	else
+		return false;
+}
+/*
+发送：
+"AT+CWMODE?
+"
+返回：
+"AT+CWMODE? +CWMODE:1
 
-		case ProtocolType::Udp:
-			return new EspUdp(*this);
+OK
+"
+*/
+AiEspMode AiESP::GetMode()
+{
+	debug_printf("GetMode\r\n");
+	String str = "AT+CWMODE?\r\n";
+	Send(str);
+	MemoryStream rsms;
+	if (RevData(rsms, 1000) == 0)return Unknown;
 
-		default:
-			return nullptr;
+	String mo1 = "+CWMODE:1";
+	String mo2 = "+CWMODE:2";
+	String mo3 = "+CWMODE:3";
+
+	String rsstr((const char*)rsms.GetBuffer(), rsms.Position());
+	Mode = Unknown;
+	if (rsstr.IndexOf(mo1) != -1)Mode = Station;
+	if (rsstr.IndexOf(mo2) != -1)Mode = Ap;
+	if (rsstr.IndexOf(mo3) != -1)Mode = Both;
+
+	debug_printf("Mode = %d\r\n", Mode);
+
+	return Mode;
+}
+// 发送数据  并按照参数条件 适当返回收到的数据
+void  AiESP::Send(Buffer & dat)
+{
+	if (dat.Length() != 0)
+	{
+		Port.Write(dat);
+		// 仅仅 send 不需要刷出去
+		//if (needRead)
+		//	Port.Flush();
 	}
 }
 
-/******************************** Socket ********************************/
-
-EspSocket::EspSocket(Esp8266& host, ProtocolType protocol)
-	: _Host(host)
+int AiESP::RevData(MemoryStream &ms, uint timeMs)
 {
-	Host		= &host;
-	Protocol	= protocol;
-}
-
-EspSocket::~EspSocket()
-{
-	Close();
-}
-
-// 应用配置，修改远程地址和端口
-void EspSocket::Change(const IPEndPoint& remote)
-{
-#if DEBUG
-	/*debug_printf("%s::Open ", Protocol == 0x01 ? "Tcp" : "Udp");
-	Local.Show(false);
-	debug_printf(" => ");
-	remote.Show(true);*/
-#endif
-
-	// 设置端口目的(远程)IP地址
-	/*SocRegWrites(DIPR, remote.Address.ToArray());
-	// 设置端口目的(远程)端口号
-	SocRegWrite2(DPORT, _REV16(remote.Port));*/
-}
-
-// 接收数据
-uint EspSocket::Receive(Buffer& bs)
-{
-	if(!Open()) return 0;
-
-	/*// 读取收到数据容量
-	ushort size = _REV16(SocRegRead2(RX_RSR));
-	if(size == 0)
+	byte temp[64];
+	Buffer bf(temp, sizeof(temp));
+	TimeWheel tw(0);
+	tw.Reset(timeMs / 1000, timeMs % 1000);
+	while (!tw.Expired())
 	{
-		// 没有收到数据时，需要给缓冲区置零，否则系统逻辑会混乱
-		bs.SetLength(0);
-		return 0;
+		// Port.Read 会修改 bf 的 length 为实际读到的数据长度 
+		// 所以每次都需要修改传入 buffer 的长度为有效值
+		Sys.Sleep(10);
+		bf.SetLength(sizeof(temp));
+		Port.Read(bf);
+		if (bf.Length() != 0)ms.Write(bf);// 把读到的数据
 	}
-
-	// 读取收到数据的首地址
-	ushort offset = _REV16(SocRegRead2(RX_RD));
-
-	// 长度受 bs 限制时 最大读取bs.Lenth
-	if(size > bs.Length()) size = bs.Length();
-
-	// 设置 实际要读的长度
-	bs.SetLength(size);
-
-	_Host.ReadFrame(offset, bs, Index, 0x03);
-
-	// 更新实际物理地址,
-	SocRegWrite2(RX_RD, _REV16(offset + size));
-	// 生效 RX_RD
-	WriteConfig(RECV);
-
-	// 等待操作完成
-	// while(ReadConfig());
-
-	//返回接收到数据的长度
-	return size;*/
-	return 0;
+	return	ms.Position();
 }
-
-// 发送数据
-bool EspSocket::Send(const Buffer& bs)
+// str 不需要换行
+bool AiESP::SendCmd(String &str, uint timeOutMs)
 {
-	if(!Open()) return false;
-	/*debug_printf("%s::Send [%d]=", Protocol == 0x01 ? "Tcp" : "Udp", bs.Length());
-	bs.Show(true);*/
+	String cmd(str);
+	cmd += "\r\n";
+	Send(cmd);
+	MemoryStream rsms;
+	auto rslen = RevData(rsms, timeOutMs);
+	debug_printf("\r\n ");
+	str.Show(false);
+	debug_printf(" SendCmd rev len:%d\r\n", rslen);
+	if (rslen == 0)return false;
 
-	/*// 读取状态
-	byte st = ReadStatus();
-	// 不在UDP  不在TCP连接OK 状态下返回
-	if(!(st == SOCK_UDP || st == SOCK_ESTABLISHE))return false;
-	// 读取缓冲区空闲大小 硬件内部自动计算好空闲大小
-	ushort remain = _REV16(SocRegRead2(TX_FSR));
-	if( remain < bs.Length())return false;
+	String rsstr((const char *)rsms.GetBuffer(), rsms.Position());
+	//rsstr.Show(true);
 
-	// 读取发送缓冲区写指针
-	ushort addr = _REV16(SocRegRead2(TX_WR));
-	_Host.WriteFrame(addr, bs, Index, 0x02);
-	// 更新发送缓存写指针位置
-	addr += bs.Length();
-	SocRegWrite2(TX_WR,_REV16(addr));
+	bool isOK = true;
+	int index = 0;
+	int indexnow = 0;
 
-	// 启动发送 异步中断处理发送异常等
-	WriteConfig(SEND);
-
-	// 控制轮询任务，加快处理
-	Sys.SetTask(_Host.TaskID, true, 20);*/
-
-	return true;
-}
-
-bool EspSocket::OnWrite(const Buffer& bs) {	return Send(bs); }
-uint EspSocket::OnRead(Buffer& bs) { return Receive(bs); }
-
-/******************************** Tcp ********************************/
-
-EspTcp::EspTcp(Esp8266& host)
-	: EspSocket(host, ProtocolType::Tcp)
-{
-	
-}
-
-/******************************** Udp ********************************/
-
-EspUdp::EspUdp(Esp8266& host)
-	: EspSocket(host, ProtocolType::Udp)
-{
-	
-}
-
-bool EspUdp::SendTo(const Buffer& bs, const IPEndPoint& remote)
-{
-	if(remote == Remote) return Send(bs);
-
-	Change(remote);
-	bool rs = Send(bs);
-	Change(Remote);
-
-	return rs;
-}
-
-bool EspUdp::OnWriteEx(const Buffer& bs, const void* opt)
-{
-	auto ep = (IPEndPoint*)opt;
-	if(!ep) return OnWrite(bs);
-
-	return SendTo(bs, *ep);
-}
-
-void EspUdp::OnProcess(byte reg)
-{
-	/*S_Interrupt ir;
-	ir.Init(reg);
-	// UDP 模式下只处理 SendOK  Recv 两种情况
-
-	if(ir.RECV)
+	index = rsstr.IndexOf(str);
+	if (index == -1)
 	{
-		RaiseReceive();
-	}*/
-	//	SEND OK   不需要处理 但要清空中断位
-}
-
-// UDP 异步只有一种情况  收到数据  可能有多个数据包
-// UDP接收到的数据结构： RemoteIP(4 byte) + RemotePort(2 byte) + Length(2 byte) + Data(Length byte)
-void EspUdp::RaiseReceive()
-{
-	/*byte buf[1500];
-	Buffer bs(buf, ArrayLength(buf));
-	ushort size = Receive(bs);
-	Stream ms(bs.GetBuffer(), size);
-
-	// 拆包
-	while(ms.Remain())
+		// 没有找到命令本身就表示不通过
+		isOK = false;
+		debug_printf("not find cmd\r\n");
+	}
+	else
 	{
-		IPEndPoint ep	= ms.ReadArray(6);
-		ep.Port = _REV16(ep.Port);
-
-		ushort len = ms.ReadUInt16();
-		len = _REV16(len);
-		// 数据长度不对可能是数据错位引起的，直接丢弃数据包
-		if(len > 1500)
+		indexnow = index + str.Length();
+		// 去掉busy
+		index = rsstr.IndexOf(busy, indexnow);
+		if (index != -1 && index - 5 < indexnow)
 		{
-			net_printf("W5500 UDP数据接收有误, ep=%s Length=%d \r\n", ep.ToString().GetBuffer(), len);
-			return;
+			// 发现有 busy 并在允许范围就跳转    没有就不动作
+			// debug_printf("find busy\r\n");
+			indexnow = index + busy.Length();
 		}
-		// 回调中断
-		Buffer bs3(ms.ReadBytes(len), len);
-		OnReceive(bs3, &ep);
-	};*/
+
+		index = rsstr.IndexOf(okstr, indexnow);
+		if (index == -1 || index - 5 > indexnow)
+		{
+			// 没有发现  或者 不在范围内就认为是错的
+			debug_printf("not find ok\r\n");
+			isOK = false;
+		}
+		else
+		{
+			// 到达这里表示 得到正确的结果了
+			indexnow = index + okstr.Length();
+		}
+	}
+	// 还有多余数据 就扔出去
+	//if(rslen -indexnow>5)xx()
+	if (isOK)
+		debug_printf(" Cmd OK\r\n");
+	else
+		debug_printf(" Cmd Fail\r\n");
+	return isOK;
 }
+/*
+发送：
+"AT+CWJAP="yws007","yws52718"
+"
+返回：	( 一般3秒钟能搞定   密码后面位置会停顿，  WIFI GOT IP 前面也会停顿 )
+"AT+CWJAP="yws007","yws52718"WIFI CONNECTED
+WIFI GOT IP
+
+OK
+"
+也可能  (已连接其他WIFI)  70bytes
+"AT+CWJAP="yws007","yws52718" WIFI DISCONNECT
+WIFI CONNECTED
+WIFI GOT IP
+
+OK
+"
+密码错误返回
+"AT+CWJAP="yws007","7" +CWJAP:1
+
+FAIL
+"
+*/
+bool AiESP::JoinAP(String & ssid, String &pwd)
+{
+	String str = "AT+CWJAP=";
+	str.Show(true);
+	str = str + "\"" + ssid + "\",\"" + pwd + "\"";
+
+	String cmd = str + "\r\n";
+
+	// 因为时间跨度很长  所以自己来捞数据
+	Send(cmd);
+	MemoryStream rsms;
+	auto rslen = RevData(rsms, 5000);	// 这里注定捞不干净  哪怕延时20s都不行  原因不明
+	debug_printf("RevData len:%d\r\n", rslen);
+	String rsstr((const char *)rsms.GetBuffer(), rsms.Position());
+	rsstr.Show(true);
+	int index = 0;
+	int indexnow = 0;
+
+	if (indexnow + 20 > rslen)			// 补捞  重组字符串   因为不动  rsms.Position() 所以数据保留不变
+	{
+		//String x = "\r\n";
+		//Send(x);
+		rslen = RevData(rsms, 1000);
+		rsstr = String((const char *)rsms.GetBuffer(), rsms.Position());
+	}
+
+	index = rsstr.IndexOf("AT+CWJAP");
+	if (index != -1)
+		indexnow = index;
+	else
+	{
+		debug_printf("not find cmd\r\n");
+		return false;
+	}
+
+	// 补捞  重组字符串   因为不动  rsms.Position() 所以数据保留不变
+	if (indexnow + 20 > rslen)
+	{
+		//String x = "\r\n";
+		//Send(x);
+		rslen = RevData(rsms, 1000);
+		rsstr = String((const char *)rsms.GetBuffer(), rsms.Position());
+	}
+	// 干掉 WIFI DISCONNECT
+	index = rsstr.IndexOf(discon, indexnow);
+	if (index != -1 /*&& index - 5 < indexnow*/)indexnow = index + discon.Length();
+
+	// 补捞  重组字符串   因为不动  rsms.Position() 所以数据保留不变
+	if (indexnow + 20 > rslen)
+	{
+		//String x = "\r\n";
+		//Send(x);
+		rslen = RevData(rsms, 1000);
+		rsstr = String((const char *)rsms.GetBuffer(), rsms.Position());
+	}
+
+	index = rsstr.IndexOf(conn, indexnow);
+
+	if (index == -1 || index - 5 > indexnow)
+	{
+		debug_printf("not find conn\r\n");
+		return false;
+	}
+	else
+	{
+		// 到达这里表示  已经连接到了
+		indexnow = index + conn.Length();
+	}
+
+	// 补捞  重组字符串   因为不动  rsms.Position() 所以数据保留不变
+	if (indexnow + 20 > rslen)
+	{
+		//String x = "\r\n";
+		//Send(x);
+		rslen = RevData(rsms, 1000);
+		rsstr = String((const char *)rsms.GetBuffer(), rsms.Position());
+	}
+
+	index = rsstr.IndexOf(gotIp, indexnow);
+	if (index == -1 || index - 5 > indexnow)
+	{
+		debug_printf("not find gotIp\r\n");
+		return false;
+	}
+	else
+	{
+		// 到达这里表示  已经连接到了
+		indexnow = index + gotIp.Length();
+	}
+
+	index = rsstr.IndexOf(okstr, indexnow);
+	if (index == -1 || index - 5 > indexnow)
+	{
+		// 没有发现  或者 不在范围内就认为是错的
+		debug_printf("not find ok\r\n");
+		return false;
+	}
+	else
+	{
+		// 到达这里表示 得到正确的结果了
+		indexnow = index + okstr.Length();
+	}
+
+	debug_printf("\r\nJoinAP  ok\r\n");
+	return true;
+}
+/*
+返回：
+"AT+CWQAP WIFI DISCONNECT
+
+OK
+"
+*/
+bool AiESP::UnJoinAP()
+{
+	String str = "AT+CWQAP";
+
+	String cmd(str);
+	cmd += "\r\n";
+	Send(cmd);
+	MemoryStream rsms;
+	auto rslen = RevData(rsms, 2000);
+	debug_printf("\r\n ");
+	str.Show(false);
+
+	debug_printf(" SendCmd rev len:%d\r\n", rslen);
+	if (rslen == 0)return false;
+	String rsstr((const char *)rsms.GetBuffer(), rsms.Position());
+	//rsstr.Show(true);
+
+	bool isOK = true;
+	int index = 0;
+	int indexnow = 0;
+
+	index = rsstr.IndexOf(str);
+	if (index == -1)
+	{
+		// 没有找到命令本身就表示不通过
+		isOK = false;
+		debug_printf("not find cmd\r\n");
+	}
+	else
+	{
+		indexnow = index + str.Length();
+		// 去掉busy
+		index = rsstr.IndexOf(busy, indexnow);
+		if (index != -1 && index - 5 < indexnow)
+		{
+			// 发现有 busy 并在允许范围就跳转    没有就不动作
+			// debug_printf("find busy\r\n");
+			indexnow = index + busy.Length();
+		}
+		// 断开连接
+		index = rsstr.IndexOf(discon, indexnow);
+		if (index != -1 && index - 5 < indexnow)
+		{
+			// 断开连接
+			indexnow = index + discon.Length();
+		}
+		// 去掉busy
+		index = rsstr.IndexOf(busy, indexnow);
+		if (index != -1 && index - 5 < indexnow)
+		{
+			indexnow = index + busy.Length();
+		}
+
+		index = rsstr.IndexOf(okstr, indexnow);
+		if (index == -1 || index - 5 > indexnow)
+		{
+			// 没有发现  或者 不在范围内就认为是错的
+			debug_printf("not find ok\r\n");
+			isOK = false;
+		}
+		else
+		{
+			// 到达这里表示 得到正确的结果了
+			indexnow = index + okstr.Length();
+		}
+	}
+	// 还有多余数据 就扔出去
+	//if(rslen -indexnow>5)xx()
+	if (isOK)
+		debug_printf(" Cmd OK\r\n");
+	else
+		debug_printf(" Cmd Fail\r\n");
+	return isOK;
+
+
+}
+/*
+开机自动连接WIFI
+*/
+bool AiESP::AutoConn(bool enable)
+{
+	String cmd = "AT+ CWAUTOCONN=";
+	if (enable)cmd += '1';
+	else cmd += '0';
+	return SendCmd(cmd);
+}
+
--- Drivers/Esp8266.h
+++ Drivers/Esp8266.h
@@ -1,33 +1,50 @@
-#ifndef __Esp8266_H__
-#define __Esp8266_H__
+﻿
+#ifndef __ESP8266_H__
+#define __ESP8266_H__
 
 #include "Sys.h"
-#include "Port.h"
+#include "SerialPort.h"
+#include "Power.h"
+#include "Time.h"
+#include "Core\Stream.h"
 #include "Net\ITransport.h"
 #include "Net\Net.h"
 
-// Zigbee协议
-// 主站发送所有从站收到，从站发送只有主站收到
-class Esp8266 : public PackPort, public ISocketHost
+
+extern void EspTest(void * param);
+
+enum AiEspMode
 {
-private:
-    OutputPort	_rst;
+	Unknown = 0,
+	Station = 1,
+	Ap = 2,
+	Both = 3,
+};
 
+// 最好打开 Soket 前 不注册中断，以免AT指令乱入到中断里面去  然后信息不对称
+// 安信可 ESP8266  模块固件版本 v1.3.0.2
+class AiESP //: public Power, public ISocketHost
+{
 public:
-	IDataPort*	Led;	// 指示灯
+	AiESP();
+	AiESP(COM com, AiEspMode mode = Unknown);
+	bool SetMode(AiEspMode mode);
 
-    Esp8266(ITransport* port, Pin rst = P0);
+	AiEspMode GetMode();
+	// timeOutMs 为等待返回数据时间  为0表示不需要管返回
+	void  Send(Buffer & dat);
+	bool SendCmd(String &str,uint timeOutMs = 1000);
 
-	//bool Open();
-	//bool Close();
-	virtual void Config();
+	int RevData(MemoryStream &ms, uint timeMs=5);
 
-	//virtual const String ToString() const { return String("Esp8266"); }
-	virtual ISocket* CreateSocket(ProtocolType type);
-
-protected:
-	virtual bool OnOpen();
-	virtual void OnClose();
+	bool JoinAP(String & ssid, String &pwd);
+	bool UnJoinAP();
+	bool AutoConn(bool enable);
+	//private:
+	SerialPort Port;
+	AiEspMode Mode = AiEspMode::Unknown;
+	// 数据回显  Write 数据被发回
+	bool BackShow = false;
 };
 
 #endif
