4b551b22f1d9674064396496e3f405ed62b66f6d
nnhy <nnhy@vip.qq.com>
2016-05-23 01:45:19 +0000

    合并8266驱动上下层代码编译通过

--- Drivers/Esp8266.cpp
+++ Drivers/Esp8266.cpp
@@ -1,11 +1,73 @@
-﻿
-#include "ESP8266.h"
+﻿#include "Time.h"
+#include "Message\DataStore.h"
 
+#include "Esp8266.h"
+
+#define NET_DEBUG DEBUG
+//#define NET_DEBUG 0
+#if NET_DEBUG
+	#define net_printf debug_printf
+#else
+	#define net_printf(format, ...)
+#endif
+
+/******************************** 内部Tcp/Udp ********************************/
+class EspSocket : public Object, public ITransport, public ISocket
+{
+private:
+	Esp8266&	_Host;
+
+public:
+	EspSocket(Esp8266& host, ProtocolType protocol);
+	virtual ~EspSocket();
+
+	// 应用配置，修改远程地址和端口
+	void Change(const IPEndPoint& remote);
+
+	virtual bool OnWrite(const Buffer& bs);
+	virtual uint OnRead(Buffer& bs);
+
+	// 发送数据
+	virtual bool Send(const Buffer& bs);
+	// 接收数据
+	virtual uint Receive(Buffer& bs);
+};
+
+class EspTcp : public EspSocket
+{
+public:
+	EspTcp(Esp8266& host);
+
+	virtual String& ToStr(String& str) const { return str + "Tcp_" + Local.Port; }
+};
+
+class EspUdp : public EspSocket
+{
+public:
+	EspUdp(Esp8266& host);
+
+	virtual bool SendTo(const Buffer& bs, const IPEndPoint& remote);
+
+	// 中断分发  维护状态
+	virtual void OnProcess(byte reg);
+	// 用户注册的中断事件处理 异步调用
+	virtual void RaiseReceive();
+
+	virtual String& ToStr(String& str) const { return str + "Udp_" + Local.Port; }
+
+private:
+	virtual bool OnWriteEx(const Buffer& bs, const void* opt);
+};
+
+/******************************** 测试 ********************************/
+#include "SerialPort.h"
 void EspTest(void * param)
 {
-	AiESP esp(COM4);
+	SerialPort sp(COM4);
+
+	Esp8266 esp(&sp);
 	//esp.Port.SetBaudRate(115200);
-	esp.Port.Open();
+	esp.Open();
 
 	Sys.Sleep(50);				//
 
@@ -35,18 +97,70 @@ String okstr = "OK";
 String gotIp = "WIFI GOT IP";
 
 
-AiESP::AiESP()
+
+/******************************** Esp8266 ********************************/
+
+Esp8266::Esp8266(ITransport* port, Pin rst)
 {
-	Mode = Unknown;
+	Set(port);
+
+	//if(rst != P0) _rst.Set(rst);
 }
 
-AiESP::AiESP(COM com, AiEspMode mode)
+bool Esp8266::OnOpen()
+{
+	if(!PackPort::OnOpen()) return false;
+
+	/*if(!_rst.Empty())
+	{
+		_rst.Open();
+
+		_rst = true;
+		Sys.Delay(100);
+		_rst = false;
+		Sys.Delay(100);
+		_rst = true;
+	}*/
+
+	return true;
+}
+
+void Esp8266::OnClose()
+{
+	//_rst.Close();
+
+	PackPort::OnClose();
+}
+
+// 配置网络参数
+void Esp8266::Config()
+{
+
+}
+
+ISocket* Esp8266::CreateSocket(ProtocolType type)
+{
+	switch(type)
+	{
+		case ProtocolType::Tcp:
+			return new EspTcp(*this);
+
+		case ProtocolType::Udp:
+			return new EspUdp(*this);
+
+		default:
+			return nullptr;
+	}
+}
+
+/*Esp8266::Esp8266(COM com, AiEspMode mode)
 {
 	Port.Set(com,115200);
 	Port.ByteTime = 1;
 	Port.MinSize = 1;
 	Mode = mode;
-}
+}*/
+
 /*
 发送：
 "AT+CWMODE=1
@@ -57,7 +171,7 @@ AiESP::AiESP(COM com, AiEspMode mode)
 OK
 "
 */
-bool AiESP::SetMode(AiEspMode mode)
+bool Esp8266::SetMode(AiEspMode mode)
 {
 	String str = "AT+CWMODE=";
 	String cmd;
@@ -87,6 +201,7 @@ bool AiESP::SetMode(AiEspMode mode)
 	else
 		return false;
 }
+
 /*
 发送：
 "AT+CWMODE?
@@ -97,7 +212,7 @@ bool AiESP::SetMode(AiEspMode mode)
 OK
 "
 */
-AiEspMode AiESP::GetMode()
+AiEspMode Esp8266::GetMode()
 {
 	debug_printf("GetMode\r\n");
 	String str = "AT+CWMODE?\r\n";
@@ -119,41 +234,46 @@ AiEspMode AiESP::GetMode()
 
 	return Mode;
 }
+
 // 发送数据  并按照参数条件 适当返回收到的数据
-void  AiESP::Send(Buffer & dat)
+void  Esp8266::Send(Buffer & dat)
 {
 	if (dat.Length() != 0)
 	{
-		Port.Write(dat);
+		Write(dat);
 		// 仅仅 send 不需要刷出去
 		//if (needRead)
 		//	Port.Flush();
 	}
 }
 
-int AiESP::RevData(MemoryStream &ms, uint timeMs)
+int Esp8266::RevData(MemoryStream &ms, uint timeMs)
 {
 	byte temp[64];
 	Buffer bf(temp, sizeof(temp));
+
 	TimeWheel tw(0);
 	tw.Reset(timeMs / 1000, timeMs % 1000);
 	while (!tw.Expired())
 	{
-		// Port.Read 会修改 bf 的 length 为实际读到的数据长度 
+		// Port.Read 会修改 bf 的 length 为实际读到的数据长度
 		// 所以每次都需要修改传入 buffer 的长度为有效值
 		Sys.Sleep(10);
 		bf.SetLength(sizeof(temp));
-		Port.Read(bf);
-		if (bf.Length() != 0)ms.Write(bf);// 把读到的数据
+		Read(bf);
+		if (bf.Length() != 0) ms.Write(bf);// 把读到的数据
 	}
+
 	return	ms.Position();
 }
+
 // str 不需要换行
-bool AiESP::SendCmd(String &str, uint timeOutMs)
+bool Esp8266::SendCmd(String &str, uint timeOutMs)
 {
 	String cmd(str);
 	cmd += "\r\n";
 	Send(cmd);
+
 	MemoryStream rsms;
 	auto rslen = RevData(rsms, timeOutMs);
 	debug_printf("\r\n ");
@@ -208,6 +328,7 @@ bool AiESP::SendCmd(String &str, uint timeOutMs)
 		debug_printf(" Cmd Fail\r\n");
 	return isOK;
 }
+
 /*
 发送：
 "AT+CWJAP="yws007","yws52718"
@@ -231,7 +352,7 @@ OK
 FAIL
 "
 */
-bool AiESP::JoinAP(String & ssid, String &pwd)
+bool Esp8266::JoinAP(String& ssid, String& pwd)
 {
 	String str = "AT+CWJAP=";
 	str.Show(true);
@@ -333,6 +454,7 @@ bool AiESP::JoinAP(String & ssid, String &pwd)
 	debug_printf("\r\nJoinAP  ok\r\n");
 	return true;
 }
+
 /*
 返回：
 "AT+CWQAP WIFI DISCONNECT
@@ -340,7 +462,7 @@ bool AiESP::JoinAP(String & ssid, String &pwd)
 OK
 "
 */
-bool AiESP::UnJoinAP()
+bool Esp8266::UnJoinAP()
 {
 	String str = "AT+CWQAP";
 
@@ -413,17 +535,198 @@ bool AiESP::UnJoinAP()
 	else
 		debug_printf(" Cmd Fail\r\n");
 	return isOK;
-
-
 }
+
 /*
 开机自动连接WIFI
 */
-bool AiESP::AutoConn(bool enable)
+bool Esp8266::AutoConn(bool enable)
 {
 	String cmd = "AT+ CWAUTOCONN=";
-	if (enable)cmd += '1';
-	else cmd += '0';
+	if (enable)
+		cmd += '1';
+	else
+		cmd += '0';
+
 	return SendCmd(cmd);
 }
 
+
+/******************************** Socket ********************************/
+
+EspSocket::EspSocket(Esp8266& host, ProtocolType protocol)
+	: _Host(host)
+{
+	Host		= &host;
+	Protocol	= protocol;
+}
+
+EspSocket::~EspSocket()
+{
+	Close();
+}
+
+// 应用配置，修改远程地址和端口
+void EspSocket::Change(const IPEndPoint& remote)
+{
+#if DEBUG
+	/*debug_printf("%s::Open ", Protocol == 0x01 ? "Tcp" : "Udp");
+	Local.Show(false);
+	debug_printf(" => ");
+	remote.Show(true);*/
+#endif
+
+	// 设置端口目的(远程)IP地址
+	/*SocRegWrites(DIPR, remote.Address.ToArray());
+	// 设置端口目的(远程)端口号
+	SocRegWrite2(DPORT, _REV16(remote.Port));*/
+}
+
+// 接收数据
+uint EspSocket::Receive(Buffer& bs)
+{
+	if(!Open()) return 0;
+
+	/*// 读取收到数据容量
+	ushort size = _REV16(SocRegRead2(RX_RSR));
+	if(size == 0)
+	{
+		// 没有收到数据时，需要给缓冲区置零，否则系统逻辑会混乱
+		bs.SetLength(0);
+		return 0;
+	}
+
+	// 读取收到数据的首地址
+	ushort offset = _REV16(SocRegRead2(RX_RD));
+
+	// 长度受 bs 限制时 最大读取bs.Lenth
+	if(size > bs.Length()) size = bs.Length();
+
+	// 设置 实际要读的长度
+	bs.SetLength(size);
+
+	_Host.ReadFrame(offset, bs, Index, 0x03);
+
+	// 更新实际物理地址,
+	SocRegWrite2(RX_RD, _REV16(offset + size));
+	// 生效 RX_RD
+	WriteConfig(RECV);
+
+	// 等待操作完成
+	// while(ReadConfig());
+
+	//返回接收到数据的长度
+	return size;*/
+	return 0;
+}
+
+// 发送数据
+bool EspSocket::Send(const Buffer& bs)
+{
+	if(!Open()) return false;
+	/*debug_printf("%s::Send [%d]=", Protocol == 0x01 ? "Tcp" : "Udp", bs.Length());
+	bs.Show(true);*/
+
+	/*// 读取状态
+	byte st = ReadStatus();
+	// 不在UDP  不在TCP连接OK 状态下返回
+	if(!(st == SOCK_UDP || st == SOCK_ESTABLISHE))return false;
+	// 读取缓冲区空闲大小 硬件内部自动计算好空闲大小
+	ushort remain = _REV16(SocRegRead2(TX_FSR));
+	if( remain < bs.Length())return false;
+
+	// 读取发送缓冲区写指针
+	ushort addr = _REV16(SocRegRead2(TX_WR));
+	_Host.WriteFrame(addr, bs, Index, 0x02);
+	// 更新发送缓存写指针位置
+	addr += bs.Length();
+	SocRegWrite2(TX_WR,_REV16(addr));
+
+	// 启动发送 异步中断处理发送异常等
+	WriteConfig(SEND);
+
+	// 控制轮询任务，加快处理
+	Sys.SetTask(_Host.TaskID, true, 20);*/
+
+	return true;
+}
+
+bool EspSocket::OnWrite(const Buffer& bs) {	return Send(bs); }
+uint EspSocket::OnRead(Buffer& bs) { return Receive(bs); }
+
+/******************************** Tcp ********************************/
+
+EspTcp::EspTcp(Esp8266& host)
+	: EspSocket(host, ProtocolType::Tcp)
+{
+
+}
+
+/******************************** Udp ********************************/
+
+EspUdp::EspUdp(Esp8266& host)
+	: EspSocket(host, ProtocolType::Udp)
+{
+
+}
+
+bool EspUdp::SendTo(const Buffer& bs, const IPEndPoint& remote)
+{
+	if(remote == Remote) return Send(bs);
+
+	Change(remote);
+	bool rs = Send(bs);
+	Change(Remote);
+
+	return rs;
+}
+
+bool EspUdp::OnWriteEx(const Buffer& bs, const void* opt)
+{
+	auto ep = (IPEndPoint*)opt;
+	if(!ep) return OnWrite(bs);
+
+	return SendTo(bs, *ep);
+}
+
+void EspUdp::OnProcess(byte reg)
+{
+	/*S_Interrupt ir;
+	ir.Init(reg);
+	// UDP 模式下只处理 SendOK  Recv 两种情况
+
+	if(ir.RECV)
+	{
+		RaiseReceive();
+	}*/
+	//	SEND OK   不需要处理 但要清空中断位
+}
+
+// UDP 异步只有一种情况  收到数据  可能有多个数据包
+// UDP接收到的数据结构： RemoteIP(4 byte) + RemotePort(2 byte) + Length(2 byte) + Data(Length byte)
+void EspUdp::RaiseReceive()
+{
+	/*byte buf[1500];
+	Buffer bs(buf, ArrayLength(buf));
+	ushort size = Receive(bs);
+	Stream ms(bs.GetBuffer(), size);
+
+	// 拆包
+	while(ms.Remain())
+	{
+		IPEndPoint ep	= ms.ReadArray(6);
+		ep.Port = _REV16(ep.Port);
+
+		ushort len = ms.ReadUInt16();
+		len = _REV16(len);
+		// 数据长度不对可能是数据错位引起的，直接丢弃数据包
+		if(len > 1500)
+		{
+			net_printf("W5500 UDP数据接收有误, ep=%s Length=%d \r\n", ep.ToString().GetBuffer(), len);
+			return;
+		}
+		// 回调中断
+		Buffer bs3(ms.ReadBytes(len), len);
+		OnReceive(bs3, &ep);
+	};*/
+}
--- Drivers/Esp8266.h
+++ Drivers/Esp8266.h
@@ -1,12 +1,8 @@
-﻿
-#ifndef __ESP8266_H__
-#define __ESP8266_H__
+﻿#ifndef __Esp8266_H__
+#define __Esp8266_H__
 
 #include "Sys.h"
-#include "SerialPort.h"
-#include "Power.h"
-#include "Time.h"
-#include "Core\Stream.h"
+#include "Port.h"
 #include "Net\ITransport.h"
 #include "Net\Net.h"
 
@@ -23,11 +19,19 @@ enum AiEspMode
 
 // 最好打开 Soket 前 不注册中断，以免AT指令乱入到中断里面去  然后信息不对称
 // 安信可 ESP8266  模块固件版本 v1.3.0.2
-class AiESP //: public Power, public ISocketHost
+class Esp8266 : public PackPort, public ISocketHost
 {
 public:
-	AiESP();
-	AiESP(COM com, AiEspMode mode = Unknown);
+	IDataPort*	Led;	// 指示灯
+
+    Esp8266(ITransport* port, Pin rst = P0);
+	//Esp8266(COM com, AiEspMode mode = Unknown);
+
+	virtual void Config();
+
+	//virtual const String ToString() const { return String("Esp8266"); }
+	virtual ISocket* CreateSocket(ProtocolType type);
+
 	bool SetMode(AiEspMode mode);
 
 	AiEspMode GetMode();
@@ -40,8 +44,14 @@ public:
 	bool JoinAP(String & ssid, String &pwd);
 	bool UnJoinAP();
 	bool AutoConn(bool enable);
-	//private:
-	SerialPort Port;
+
+
+protected:
+	virtual bool OnOpen();
+	virtual void OnClose();
+
+private:
+    OutputPort	_rst;
 	AiEspMode Mode = AiEspMode::Unknown;
 	// 数据回显  Write 数据被发回
 	bool BackShow = false;
