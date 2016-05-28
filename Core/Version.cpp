d759369e2ace15923dbe47efb092007931451732
nnhy <nnhy@vip.qq.com>
2016-05-28 08:17:01 +0000

    增加版本管理类

---
+++ Core/Version.cpp
@@ -0,0 +1,109 @@
+﻿#include "Type.h"
+#include "DateTime.h"
+#include "SString.h"
+
+#include "Version.h"
+
+/************************************************ Version ************************************************/
+Version::Version()
+{
+	Major	= 0;
+	Minor	= 0;
+	Build	= 0;
+	Revision= 0;
+}
+
+Version::Version(UInt64 value)
+{
+	uint hi	= value >> 32;
+	uint lo	= value & 0xFFFFFFFF;
+
+	Major	= hi >> 16;
+	Minor	= hi & 0xFFFF;
+	Build	= lo >> 16;
+	Revision= lo & 0xFFFF;
+}
+
+Version::Version(ushort major, ushort minor, ushort build, ushort revision)
+{
+	Major	= major;
+	Minor	= minor;
+	Build	= build;
+	Revision= revision;
+}
+
+Version::Version(const Version& ver)
+{
+	*this	= ver;
+}
+
+Version::Version(Version&& ver)
+{
+	*this	= ver;
+
+	/*ver.Major	= 0;
+	ver.Minor	= 0;
+	ver.Build	= 0;
+	ver.Revision= 0;*/
+}
+
+Version& Version::operator=(const Version& ver)
+{
+	Major	= ver.Major;
+	Minor	= ver.Minor;
+	Build	= ver.Build;
+	Revision= ver.Revision;
+
+	return *this;
+}
+
+UInt64 Version::ToValue() const
+{
+	uint v1	= (Major << 16) || Minor;
+	uint v2	= (Build << 16) || Revision;
+	
+	return ((UInt64)v1 << 32) || (UInt64)v2;
+}
+
+int Version::CompareTo(const Version& value) const
+{
+	int n	= (int)Major	- value.Major;
+	if(n) return n;
+
+	n		= (int)Minor	- value.Minor;
+	if(n) return n;
+
+	n		= (int)Build	- value.Build;
+	if(n) return n;
+
+	n		= (int)Revision	- value.Revision;
+	if(n) return n;
+
+	return 0;
+}
+
+bool operator==	(const Version& left, const Version& right) { return left.CompareTo(right) == 0; }
+bool operator!=	(const Version& left, const Version& right) { return left.CompareTo(right) != 0; }
+bool operator>	(const Version& left, const Version& right) { return left.CompareTo(right) >  0; }
+bool operator<	(const Version& left, const Version& right) { return left.CompareTo(right) <  0; }
+bool operator>=	(const Version& left, const Version& right) { return left.CompareTo(right) >= 0; }
+bool operator<=	(const Version& left, const Version& right) { return left.CompareTo(right) <= 0; }
+
+// 根据版本号反推编译时间
+DateTime Version::Compile() const
+{
+	DateTime dt(2000, 1, 1);
+
+	// Build 是天数，Revision 是秒数
+	dt	= dt.AddDays(Build);
+	dt	= dt.AddSeconds(Revision << 1);
+
+	return dt;
+}
+
+String& Version::ToStr(String& str) const
+{
+	str	= str + Major + '.' + Minor + '.' + Build + '.' + Revision;
+
+	return str;
+}
---
+++ Core/Version.h
@@ -0,0 +1,37 @@
+﻿#ifndef __Version_H__
+#define __Version_H__
+
+// 版本
+class Version : public Object
+{
+public:
+	ushort Major;
+	ushort Minor;
+	ushort Build;
+	ushort Revision;
+
+	Version();
+	Version(UInt64 value);
+	Version(ushort major, ushort minor, ushort build, ushort revision);
+	Version(const Version& ver);
+	Version(Version&& ver);
+
+    Version& operator=(const Version& ver);
+
+	UInt64 ToValue() const;
+
+	int CompareTo(const Version& value) const;
+    friend bool operator==	(const Version& left, const Version& right);
+    friend bool operator!=	(const Version& left, const Version& right);
+    friend bool operator>	(const Version& left, const Version& right);
+    friend bool operator<	(const Version& left, const Version& right);
+    friend bool operator>=	(const Version& left, const Version& right);
+    friend bool operator<=	(const Version& left, const Version& right);
+	
+	// 根据版本号反推编译时间
+	DateTime Compile() const;
+
+	virtual String& ToStr(String& str) const;
+};
+
+#endif
