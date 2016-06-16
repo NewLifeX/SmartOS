fa9f3321a3148704d6c5877db8ddf5d84094c397
Stone <nnhy@vip.qq.com>
2016-06-16 09:33:58 +0000

    等待时间计算为当前任务的睡眠时间

--- Kernel/WaitHandle.cpp
+++ Kernel/WaitHandle.cpp
@@ -11,12 +11,16 @@ WaitHandle::WaitHandle()
 bool WaitHandle::WaitOne(int ms)
 {
 	auto host	= Task::Scheduler();
+	// 记录当前正在执行任务
+	auto task = host->Current;
 
 	// 实际可用时间，-1表示无限等待
 	if(ms == -1) ms	= 0x7FFFFFFF;
 
+	TimeCost tc;
+
 	auto now	= Sys.Ms();
-	
+
 	// 如果休眠时间足够长，允许多次调度其它任务
 	while(ms > 0)
 	{
@@ -27,6 +31,11 @@ bool WaitHandle::WaitOne(int ms)
 
 		now	= Sys.Ms();
 		ms -= (int)(now - start);
+	}
+	if(task)
+	{
+		host->Current	= task;
+		task->SleepTime	+= tc.Elapsed();
 	}
 
 	return Result;
