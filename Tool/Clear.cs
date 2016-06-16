4057881a4fe268c866757902b72adee6adeac61b
WangQiang <807319473@qq.com>
2016-06-16 15:40:50 +0000

    添加删除中间文件的脚本

---
+++ Tool/Clear.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.Win32;
+using NewLife.Log;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Xml;
+using NewLife.Web;
+
+namespace NewLife.Reflection
+{
+    public class ScriptEngine
+    {
+        static void Main()
+        {
+			if(Directory.Exists("ObjD"))Directory.Delete("ObjD",true);
+			if(Directory.Exists("Obj"))Directory.Delete("Obj",true);
+        }
+    }
+}
