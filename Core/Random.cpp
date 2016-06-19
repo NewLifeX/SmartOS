c449086702a1ed8126466845a72d7021aafa59a6
Stone <nnhy@vip.qq.com>
2016-06-19 10:49:14 +0000

    增加随机数

---
+++ Core/Random.cpp
@@ -0,0 +1,33 @@
+﻿#include <stdlib.h>
+#include <time.h>
+
+#include "Buffer.h"
+
+#include "Random.h"
+
+/************************************************ Random ************************************************/
+Random::Random()
+{
+	srand(time(NULL));
+}
+
+Random::Random(uint seed)
+{
+	srand(seed);
+}
+
+uint Random::Next() const
+{
+	return rand();
+}
+
+uint Random::Next(uint max) const
+{
+	return rand() % max;
+}
+
+void Random::Next(Buffer& bs) const
+{
+	for(int i=0; i<bs.Length(); i++)
+		bs[i]	= rand() & 0xFF;
+}
---
+++ Core/Random.h
@@ -0,0 +1,16 @@
+﻿#ifndef __Random_H__
+#define __Random_H__
+
+// 随机数
+class Random
+{
+public:
+	Random();
+	Random(uint seed);
+
+	uint Next() const;
+	uint Next(uint max) const;
+	void Next(Buffer& bs) const;
+};
+
+#endif
