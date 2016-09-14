f9b76d45c975f75fd49711915975fbc3e74d0820
nnhy <nnhy@vip.qq.com>
2016-09-14 10:55:22 +0000

    Json取值测试未通过

--- Test/JsonTest.cpp
+++ Test/JsonTest.cpp
@@ -10,11 +10,11 @@ static void TestRead()
 	\"name\": \"Stone\",	\
 	\"enable\": true,		\
 	\"noval\": null,		\
-	\"score\": 3.141,		\
+	\"score\": 3.1415,		\
 	\"array\": [1, 0, 2],	\
 	\"extend\": {			\
 		\"kind\": \"cost\",	\
-		\"value\": 42.99	\
+		\"value\": 67.89	\
 	}\
 }";
 
@@ -22,24 +22,37 @@ static void TestRead()
 
 	auto id		= json["id"];
 	assert(id.Type() == JsonType::integer, "Type()");
+	assert(id.AsInt() == 3141, "AsInt()");
 
 	auto name	= json["name"];
 	assert(name.Type() == JsonType::string, "Type()");
+	assert(name.AsString() == "Stone", "AsString()");
 
 	auto enable	= json["enable"];
 	assert(enable.Type() == JsonType::boolean, "Type()");
+	assert(enable.AsBoolean() == true, "AsBoolean()");
 
 	auto noval	= json["noval"];
 	assert(noval.Type() == JsonType::null, "Type()");
 
 	auto score	= json["score"];
 	assert(score.Type() == JsonType::Float, "Type()");
+	assert(score.AsFloat() == 3.1415, "AsFloat()");
 
 	auto array	= json["array"];
 	assert(array.Type() == JsonType::array, "Type()");
+	assert(score.Length() == 3, "Length()");
 
 	auto extend	= json["extend"];
 	assert(extend.Type() == JsonType::object, "Type()");
+
+	auto kind	= extend["kind"];
+	assert(kind.Type() == JsonType::string, "Type()");
+	assert(kind.AsString() == "cost", "AsString()");
+
+	auto value	= extend["value"];
+	assert(value.Type() == JsonType::Float, "Type()");
+	assert(value.AsFloat() == 67.89, "AsFloat()");
 }
 
 void Json::Test()
