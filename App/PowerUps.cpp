cc85dd9afb03c35db43b131f58f4634143a38a25
WangQiang <807319473@qq.com>
2016-10-10 07:37:20 +0000

    短时间重启计数功能

---
+++ App/PowerUps.cpp
@@ -0,0 +1,65 @@
+
+#include "PowerUps.h"
+#include "../Config.h"
+
+
+class PowerUpsCfg : public ConfigBase
+{
+public:
+	byte	Length;			// 数据长度
+	byte	ReStartCount;	// 重启次数
+	byte	TagEnd;			// 数据区结束标识符
+
+	PowerUpsCfg();
+
+	virtual void Init();
+};
+
+PowerUpsCfg::PowerUpsCfg() : ConfigBase()
+{
+	_Name = "PowUps";
+	_Start = &Length;
+	_End = &TagEnd;
+	Init();
+}
+
+void PowerUpsCfg::Init()
+{
+	Length = Size();
+	ReStartCount = 0;
+}
+
+/****************************************************************/
+/****************************************************************/
+
+PowerUps::PowerUps(byte thld, int timeMs, Func act)
+{
+	ReStThld = thld;
+	if (act)		// 没有动作 直接忽略Flash操作
+	{
+		PowerUpsCfg pc;
+		pc.Load();
+
+		ReStartCount = ++pc.ReStartCount;
+		pc.Save();
+	}
+	// 保留他处理一些事情
+	Sys.AddTask(DelayAct, this, timeMs, -1, "重启阈值");
+}
+
+void PowerUps::DelayAct(void* param)
+{
+	auto* pu = (PowerUps*) param;
+	if (pu->Act)
+	{
+		if (pu->ReStartCount > pu->ReStThld)pu->Act();
+		//pu->ReStartCount = 0;
+
+		PowerUpsCfg pc;
+		pc.Load();
+		pc.ReStartCount = 0;
+		pc.Save();
+	}
+	// 完成使命 功成身退
+	delete pu;
+}
---
+++ App/PowerUps.h
@@ -0,0 +1,29 @@
+﻿
+#ifndef __Music_H__
+#define __Music_H__
+
+#include "Sys.h"
+#include "../Core/Delegate.h"
+
+
+/*
+	每次启动，FLASH 记录重启数加1。延迟5秒清零。
+	如果没有清零，表示 timems 秒内有第二次重启。
+	当有多次重启后可以判断。某段时间内
+*/
+
+
+class PowerUps
+{
+public:	
+	PowerUps(byte thld,int timeMs = 5, Func act = nullptr);
+
+	byte ReStartCount;	// 当前重启的次数
+	byte ReStThld;		// 重启阈值，超过某个值则处理某事
+
+	Func Act;			// 当重启次数超出阈值时执行的动作
+
+	static void DelayAct(void *param);	// 延迟动作
+};
+
+#endif 
