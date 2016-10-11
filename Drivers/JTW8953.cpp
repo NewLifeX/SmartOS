5eb8acf9484cb605892b6ced9a720083f52f1b2a
cdyong <273797002@qq.com>
2016-10-11 08:36:24 +0000

    不需偏移一位

--- Drivers/JTW8953.cpp
+++ Drivers/JTW8953.cpp
@@ -4,7 +4,7 @@
 #define ADDRESS_R		0xa7	//从机的地址和读取标志
 
 #define ADDRESS_C		0xB1	// MCU配置起始地址
-#define ADDRESS_T		0xC0	// 触摸键起始地址, 共有10个触摸键地址范围0xc0~0xD0
+#define ADDRESS_T		0xC0	// 触摸键起始地址, 共有9个触摸键地址范围0xc0~0xD0
 
 JTW8953::JTW8953()
 {
@@ -24,7 +24,7 @@ void JTW8953::Init()
 	debug_printf("\r\nJTW8953::Init Address=0x%02X \r\n", Address);
 
 	IIC->SubWidth	= 1;
-	IIC->Address	= Address << 1;
+	IIC->Address	= Address;
 }
 
 bool JTW8953::SetConfig(const Buffer& bs) const
@@ -32,7 +32,7 @@ bool JTW8953::SetConfig(const Buffer& bs) const
    return Write(ADDRESS_C, bs);
 }
 
-bool JTW8953::Write(ushort addr, byte data)
+bool JTW8953::WriteKey(ushort addr, byte data)
 {
 	if(!IIC) return false;
 	// 只有10个按键
@@ -53,14 +53,14 @@ byte JTW8953::Read(ushort addr)
 	IIC->Address = ADDRESS_R;
 	addr = addr + ADDRESS_T;
 
-	return IIC->Read(addr & 0xFF);
+	return IIC->Read(addr);
 }
 
 bool JTW8953::Write(uint addr, const Buffer& bs) const
 {
 	if(!IIC) return false;
 	
-	IIC->Address = ADDRESS_W << 1;
+	IIC->Address = ADDRESS_W;
 
 	return IIC->Write((ushort)addr, bs);
 }
@@ -69,7 +69,7 @@ bool JTW8953::Read(uint addr, Buffer& bs) const
 {
 	if(!IIC) return false;
 
-	IIC->Address = ADDRESS_R << 1;
+	IIC->Address = ADDRESS_R;
 	
 	uint len = IIC->Read((ushort)addr, bs);
 	if(len == 0)return false;
