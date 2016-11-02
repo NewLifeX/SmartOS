b666bb8b7cceea39b1310b3d0e8c08aefcc107c5
cdyong <273797002@qq.com>
2016-11-02 04:01:54 +0000

    新功能需要添加抖动时间

--- Board/AP0801.cpp
+++ Board/AP0801.cpp
@@ -116,6 +116,7 @@ void AP0801::InitButtons(const Delegate2<InputPort&, bool>& press)
 	{
 		auto port = new InputPort(ButtonPins[i]);
 		port->Invert = true;
+		port->ShakeTime = 40;
 		port->State	= i;
 		port->Press	= press;
 		port->UsePress();
