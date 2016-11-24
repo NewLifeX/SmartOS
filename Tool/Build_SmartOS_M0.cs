a79a87d736d63d180e4f257ae6a0a9404dd2c3bc
大石头 <nnhy@vip.qq.com>
2016-11-25 01:43:46 +0800

    升级编译脚本

--- Tool/Build_SmartOS_M0.cs
+++ Tool/Build_SmartOS_M0.cs
@@ -1,51 +1,27 @@
-using System;
-using System.Collections;
-using System.Diagnostics;
-using System.Reflection;
-using System.Text;
-using System.Linq;
-using System.IO;
-using System.Collections.Generic;
-using Microsoft.Win32;
-using NewLife.Log;
+var build = Builder.Create("MDK");
+build.Init();
+build.CPU = "Cortex-M0+";
+build.Defines.Add("STM32F0");
+build.AddIncludes("..\\", false);
+build.AddFiles("..\\Core");
+build.AddFiles("..\\Kernel");
+build.AddFiles("..\\Device");
+build.AddFiles("..\\", "*.c;*.cpp", false);
+build.AddFiles("..\\Security", "*.cpp");
+build.AddFiles("..\\Board");
+build.AddFiles("..\\Storage");
+build.AddFiles("..\\App");
+build.AddFiles("..\\Drivers");
+build.AddFiles("..\\Net");
+build.AddFiles("..\\Test");
+build.AddFiles("..\\TinyIP", "*.c;*.cpp", false, "HttpClient");
+build.AddFiles("..\\Message");
+build.AddFiles("..\\TinyNet");
+build.AddFiles("..\\TokenNet");
+build.Libs.Clear();
+build.CompileAll();
+build.BuildLib("..\\SmartOS_M0");
 
-namespace NewLife.Reflection
-{
-    public class ScriptEngine
-    {
-        static void Main()
-        {
-            var build = Builder.Create("MDK");
-            build.Init();
-			build.Cortex = 0;
-			build.Defines.Add("STM32F0");
-			build.AddIncludes("..\\", false);
-            build.AddFiles("..\\Core");
-            build.AddFiles("..\\Kernel");
-            build.AddFiles("..\\Device");
-            build.AddFiles("..\\", "*.c;*.cpp", false);
-            build.AddFiles("..\\Security", "*.cpp");
-            build.AddFiles("..\\Board");
-            build.AddFiles("..\\Storage");
-            build.AddFiles("..\\App");
-            build.AddFiles("..\\Drivers");
-            build.AddFiles("..\\Net");
-            build.AddFiles("..\\Test");
-            build.AddFiles("..\\TinyIP", "*.c;*.cpp", false, "HttpClient");
-            build.AddFiles("..\\Message");
-            build.AddFiles("..\\TinyNet");
-            build.AddFiles("..\\TokenNet");
-			build.Libs.Clear();
-            build.CompileAll();
-            build.BuildLib("..\\SmartOS_M0");
-
-			build.Debug = true;
-            build.CompileAll();
-            build.BuildLib("..\\SmartOS_M0");
-
-			/*build.Tiny = true;
-            build.CompileAll();
-            build.BuildLib("..\\SmartOS_M0");*/
-        }
-    }
-}
+build.Debug = true;
+build.CompileAll();
+build.BuildLib("..\\SmartOS_M0");
--- Tool/Build_SmartOS_M4.cs
+++ Tool/Build_SmartOS_M4.cs
@@ -1,51 +1,27 @@
-using System;
-using System.Collections;
-using System.Diagnostics;
-using System.Reflection;
-using System.Text;
-using System.Linq;
-using System.IO;
-using System.Collections.Generic;
-using Microsoft.Win32;
-using NewLife.Log;
+var build = Builder.Create("MDK");
+build.Init();
+build.CPU = "Cortex-M4";
+build.Defines.Add("STM32F4");
+build.AddIncludes("..\\", false);
+build.AddFiles("..\\Core");
+build.AddFiles("..\\Kernel");
+build.AddFiles("..\\Device");
+build.AddFiles("..\\", "*.c;*.cpp", false);
+build.AddFiles("..\\Net");
+build.AddFiles("..\\Message");
+build.AddFiles("..\\Security", "*.cpp");
+build.AddFiles("..\\Board");
+build.AddFiles("..\\Storage");
+build.AddFiles("..\\App");
+build.AddFiles("..\\Drivers");
+build.AddFiles("..\\Test");
+build.AddFiles("..\\TinyIP", "*.c;*.cpp", false, "HttpClient");
+build.AddFiles("..\\TinyNet");
+build.AddFiles("..\\TokenNet");
+build.Libs.Clear();
+build.CompileAll();
+build.BuildLib("..\\SmartOS_M4");
 
-namespace NewLife.Reflection
-{
-    public class ScriptEngine
-    {
-        static void Main()
-        {
-            var build = Builder.Create("MDK");
-            build.Init();
-			build.Cortex = 4;
-			build.Defines.Add("STM32F4");
-			build.AddIncludes("..\\", false);
-            build.AddFiles("..\\Core");
-            build.AddFiles("..\\Kernel");
-            build.AddFiles("..\\Device");
-            build.AddFiles("..\\", "*.c;*.cpp", false);
-            build.AddFiles("..\\Security", "*.cpp");
-            build.AddFiles("..\\Board");
-            build.AddFiles("..\\Storage");
-            build.AddFiles("..\\App");
-            build.AddFiles("..\\Drivers");
-            build.AddFiles("..\\Net");
-            build.AddFiles("..\\Test");
-            build.AddFiles("..\\TinyIP", "*.c;*.cpp", false, "HttpClient");
-            build.AddFiles("..\\Message");
-            build.AddFiles("..\\TinyNet");
-            build.AddFiles("..\\TokenNet");
-			build.Libs.Clear();
-            build.CompileAll();
-            build.BuildLib("..\\SmartOS_M4");
-
-			build.Debug = true;
-            build.CompileAll();
-            build.BuildLib("..\\SmartOS_M4");
-
-			/*build.Tiny = true;
-            build.CompileAll();
-            build.BuildLib("..\\SmartOS_M4");*/
-        }
-    }
-}
+build.Debug = true;
+build.CompileAll();
+build.BuildLib("..\\SmartOS_M4");
