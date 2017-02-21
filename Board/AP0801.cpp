921e42e59773821cb51059db04a4efb6efe33c8e
cdy <cdy@wslink.cn>
2017-02-21 09:56:46 +0800

    低功耗对WIFI配置有影响

--- Board/AP0801.cpp
+++ Board/AP0801.cpp
@@ -60,7 +60,7 @@ void AP0801::Init(ushort code, cstring name, COM message)
 	WatchDog::Start();
 
 	// 系统休眠时自动进入低功耗
-	Power::AttachTimeSleep();
+	// Power::AttachTimeSleep();
 #endif
 
 	// Flash最后一块作为配置区
