12fdd023f778b1f1c4d6aa3512f9702ebc22e710
大石头 <nnhy@vip.qq.com>
2017-03-25 12:36:00 +0800

    SDK分二级目录

--- Tool/发布SDK.cs
+++ Tool/发布SDK.cs
@@ -1,13 +1,16 @@
 ﻿var dst = "..\\..\\SmartSDK\\";
-"..\\".AsDirectory().CopyToIfNewer(dst, "*.h;*.exe;*.dll", true);
-"..\\Test".AsDirectory().CopyToIfNewer(dst + "Test", "*.cpp");
-//Directory.Delete(dst + "Platform", true);
-//Directory.Delete(dst + "XX", true);
+var dsos = dst + "SmartOS\\";
+var dlib = dst + "Lib\\";
+
+"..\\".AsDirectory().CopyTo(dsos, "*.h;*.exe;*.dll", true);
+"..\\Test".AsDirectory().CopyTo(dsos + "Test", "*.cpp");
+Directory.Delete(dsos + "Platform", true);
+Directory.Delete(dsos + "XX", true);
 
 // 压缩库文件
 foreach(var item in "..\\".AsDirectory().GetAllFiles("*.lib;*.a"))
 {
-	var dz = (dst + item.Name + ".7z").AsFile();
+	var dz = (dsos + item.Name + ".7z").AsFile();
 	if(!dz.Exists || dz.LastWriteTime < item.LastWriteTime)
 	{
 		Console.WriteLine("压缩 {0}", item.Name);
@@ -16,7 +19,7 @@ foreach(var item in "..\\".AsDirectory().GetAllFiles("*.lib;*.a"))
 }
 foreach(var item in "..\\..\\Lib".AsDirectory().GetAllFiles("*.lib;*.a"))
 {
-	var dz = (dst + item.Name + ".7z").AsFile();
+	var dz = (dlib + item.Name + ".7z").AsFile();
 	if(!dz.Exists || dz.LastWriteTime < item.LastWriteTime)
 	{
 		Console.WriteLine("压缩 {0}", item.Name);
