5c040b05552a74828d1596095b1feb2c0a659d4b
大石头 <nnhy@vip.qq.com>
2017-10-28 14:42:54 +0800

    增加编译脚本，把M3+F1+LibF1编译到一个库里面。但是很不方便使用。 1，加大了编译耗时，超过24小时后，即使文件没有被修改，也要重新编译； 2，不能混合使用调试版和发行版，比如LibF1一向比较稳定，实际项目调试版也可能使用它的发行版以减小固件大小； 3，静态库lib里面会出现多个同名文件，比如SerialPort.o就有3个

---
+++ Tool/Build_SmartOS_F1.cs
@@ -0,0 +1,26 @@
+var build = Builder.Create("MDK");
+build.Init(false);
+build.CPU = "Cortex-M3";
+build.Output = "F1";
+
+build.Defines.Add("STM32F1");
+//include=_Files.cs
+
+build.AddIncludes("..\\..\\LibF1\\CMSIS");
+build.AddIncludes("..\\..\\LibF1\\Inc");
+build.AddIncludes("..\\Platform", false);
+build.AddFiles("..\\Platform\\STM32F1", "*.c;*.cpp;startup_stm32f10x.s");
+build.AddFiles("..\\Platform\\CortexM", "*.c;*.cpp");
+
+build.AddFiles("..\\..\\LibF1", "*.c;*.cpp", true, "system_stm32f10x");
+build.AddIncludes("..\\..\\LibF1\\cmsis", false);
+build.AddIncludes("..\\..\\LibF1\\inc", false);
+build.Defines.Add("USE_STDPERIPH_DRIVER");
+
+build.Libs.Clear();
+build.CompileAll();
+build.BuildLib("..\\SmartOS_F1");
+
+build.Debug = true;
+build.CompileAll();
+build.BuildLib("..\\SmartOS_F1");
